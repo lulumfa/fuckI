033eab38369dd63a07ce38f24019c308ffe2ee0b
fucktheinterview <jzy0706@vip.163.com>
2014-03-09 18:05:46 -0400

    Update LRUCache.java

--- Algorithms/LRUCache.java
+++ Algorithms/LRUCache.java
@@ -1,97 +1,95 @@
-import java.util.*;
-
+import java.util.HashMap;
+ 
 public class LRUCache {
-        private Map<Integer, DLinkedItem> dataMap;
-        private int capacity;
-        private DLinkedList dLinkedList;
-        
-        public LRUCache(int capacity) {
-            dLinkedList = new DLinkedList();
-            dataMap = new HashMap<Integer, DLinkedItem>(capacity);
-            this.capacity = capacity;
-        }
+	private HashMap<Integer, DoubleLinkedListNode> map 
+		= new HashMap<Integer, DoubleLinkedListNode>();
+	private DoubleLinkedListNode head;
+	private DoubleLinkedListNode end;
+	private int capacity;
+	private int len;
  
-        public int get(int key) {
-            if (dataMap.containsKey(key)) {
-                DLinkedItem d = dataMap.get(key);
-                dLinkedList.moveToFront(d);
-                return d.val;
-            }
-            else {
-                return -1;
-            }
-        }
-     
-        public void set(int key, int value) {
-            if (dataMap.containsKey(key)) {
-                DLinkedItem d = dataMap.get(key);
-                d.val = value;
-                dLinkedList.moveToFront(d);
-            }
-            else {
-                if (dataMap.size() < capacity) { // addToFront
-                    DLinkedItem d = new DLinkedItem(value, key);
-                    dataMap.put(key, d);
-                    dLinkedList.addToFront(d);
-                }
-                else { 
-                    DLinkedItem d = new DLinkedItem(value, key);
-                    int rmKey = dLinkedList.removeLeast();
-                    dataMap.remove(rmKey);
-                    dataMap.put(key, d);
-                    dLinkedList.addToFront(d);
-                }
-            }
-        }
-     
-      
-        class DLinkedItem {
-            public DLinkedItem(int val, int k) {
-                this.val = val;
-                this.k = k;
-            }
-            public int getK() { return k; }
-         
-            public DLinkedItem prev;
-            public DLinkedItem next;
-            public int val;
-            private int k;
-        }
+	public LRUCache(int capacity) {
+		this.capacity = capacity;
+		len = 0;
+	}
  
-        class DLinkedList {
-            public DLinkedItem front, end;
-         
-            public DLinkedList() {
-                front = new DLinkedItem(-9999, -9999);
-                end = new DLinkedItem(-9999, -9999);
-                end.next = front;
-                front.prev = end;
-            }
-         
-            public void moveToFront(DLinkedItem d) {
-                d.prev.next = d.next;
-                d.next.prev = d.prev;
-                front.prev.next = d;
-                d.prev = front.prev;
-                front.prev = d;
-                d.next = front;
-            }
-         
-            public void addToFront(DLinkedItem d) {
-                front.prev.next = d;
-                d.prev = front.prev;
-                front.prev = d;
-                d.next = front;
-            }
-         
-            public int removeLeast() {
-                DLinkedItem least = end.next;
-                int res = least.getK();
-                end.next = least.next;
-                least.next.prev = end;
-                return res;
-            }
-         
-    
-         }
+	public int get(int key) {
+		if (map.containsKey(key)) {
+			DoubleLinkedListNode latest = map.get(key);
+			removeNode(latest);
+			setHead(latest);
+			return latest.val;
+		} else {
+			return -1;
+		}
+	}
+ 
+	public void removeNode(DoubleLinkedListNode node) {
+		DoubleLinkedListNode cur = node;
+		DoubleLinkedListNode pre = cur.pre;
+		DoubleLinkedListNode post = cur.next;
+ 
+		if (pre != null) {
+			pre.next = post;
+		} else {
+			head = post;
+		}
+ 
+		if (post != null) {
+			post.pre = pre;
+		} else {
+			end = pre;
+		}
+	}
+ 
+	public void setHead(DoubleLinkedListNode node) {
+		node.next = head;
+		node.pre = null;
+		if (head != null) {
+			head.pre = node;
+		}
+ 
+		head = node;
+		if (end == null) {
+			end = node;
+		}
+	}
+ 
+	public void set(int key, int value) {
+		if (map.containsKey(key)) {
+			DoubleLinkedListNode oldNode = map.get(key);
+			oldNode.val = value;
+			removeNode(oldNode);
+			setHead(oldNode);
+		} else {
+			DoubleLinkedListNode newNode = 
+				new DoubleLinkedListNode(key, value);
+			if (len < capacity) {
+				setHead(newNode);
+				map.put(key, newNode);
+				len++;
+			} else {
+				map.remove(end.key);
+				end = end.pre;
+				if (end != null) {
+					end.next = null;
+				}
+ 
+				setHead(newNode);
+				map.put(key, newNode);
+			}
+		}
+	}
+}
+ 
+class DoubleLinkedListNode {
+	public int val;
+	public int key;
+	public DoubleLinkedListNode pre;
+	public DoubleLinkedListNode next;
+ 
+	public DoubleLinkedListNode(int key, int value) {
+		val = value;
+		this.key = key;
+	}
 }

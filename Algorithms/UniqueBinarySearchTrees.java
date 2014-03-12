3d78fcd16540783470e6b2cb80b7f7eb58bfdd23
fucktheinterview <jzy0706@vip.163.com>
2014-03-12 12:50:58 -0400

    Create UniqueBinarySearchTrees.java

---
+++ Algorithms/UniqueBinarySearchTrees.java
@@ -0,0 +1,25 @@
+// Note 1. 第一种方法，用的DP。 第二种方法，考虑f(n) 和 f(n-1) 的关系， 很直接。
+// Link : 阿三哥有个视频链接，讲这题，讲的很清楚，http://www.youtube.com/watch?v=UfA_v0VmiDg
+//           不得不说，阿三哥做视频有一套，经常看youtube的视频都是三哥做的，讲的很清楚，印度英语差点就是了。
+
+public int numTrees(int n) {
+                        if(n == 0) return 1;
+                        if(n == 1) return 1;
+                        
+                        int[] f = new int[n+1];
+                        f[0] = 1;
+                        f[1] = 1;
+                        for(int i = 2; i < n+1; i++) {
+                             for(int j = 1 ; j <= i; j++) {
+                                    f[i] += f[i-j] * f[j-1];
+                             }
+                        }
+                        return f[n];
+                 } 
+                 
+                  public int numTrees(int n) { 
+                     int c = 1; 
+                     for (int i = 2; i <= n; i++) 
+                            c = 2*(2*i-1)*c/(i+1);
+                     return c;
+                }

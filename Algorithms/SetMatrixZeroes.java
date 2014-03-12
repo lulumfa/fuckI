c4c7ed8a501c91b7716f2dac9cad96095df6ec94
fucktheinterview <jzy0706@vip.163.com>
2014-03-12 13:32:30 -0400

    Create SetMatrixZeroes.java

---
+++ Algorithms/SetMatrixZeroes.java
@@ -0,0 +1,61 @@
+// http://www.programcreek.com/2012/12/leetcode-set-matrix-zeroes-java/
+
+// This problem can solve by following 4 steps:
+
+// 1. check if first row and column are zero or not
+// 2. mark zeros on first row and column
+// 3. use mark to set elements
+// 4. rst column and row by using marks in step 1
+
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstRowZero = false;
+        boolean firstColumnZero = false;
+ 
+        //set first row and column zero or not
+        for(int i=0; i<matrix.length; i++){
+            if(matrix[i][0] == 0){
+                firstColumnZero = true;
+                break;
+            }
+        }
+ 
+        for(int i=0; i<matrix[0].length; i++){
+            if(matrix[0][i] == 0){
+                firstRowZero = true;
+                break;
+            }
+        }
+ 
+        //mark zeros on first row and column
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                   matrix[i][0] = 0;
+                   matrix[0][j] = 0;
+                }
+            }
+        }
+ 
+        //use mark to set elements
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[0].length; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                   matrix[i][j] = 0;
+                }
+            }
+        }
+ 
+        //set first column and row
+        if(firstColumnZero){
+            for(int i=0; i<matrix.length; i++)
+                matrix[i][0] = 0;
+        }
+ 
+        if(firstRowZero){
+            for(int i=0; i<matrix[0].length; i++)
+                matrix[0][i] = 0;
+        }
+ 
+    }
+}

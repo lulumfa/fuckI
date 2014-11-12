9703f66408de9defc8125a77dbf8ac7e3d161e98
fucktheinterview <jzy0706@vip.163.com>
2014-11-12 09:30:54 -0800

    Update MinStack.java

--- Algorithms/MinStack.java
+++ Algorithms/MinStack.java
@@ -20,3 +20,43 @@ class MinStack {
         return minStack.peek();
     }
 }
+
+// not using Java built-in Stack and with 2 ListNodes
+class MinStack {
+    ListNode stackHead;
+    ListNode minStackHead; 
+    public void push(int x) {
+        if(minStackHead==null || x<=minStackHead.val) {
+            ListNode temp1 = new ListNode(x);
+            temp1.next = minStackHead;
+            minStackHead = temp1;
+        }
+        ListNode temp2 = new ListNode(x);
+        temp2.next = stackHead;
+        stackHead = temp2;
+    }
+
+    public void pop() {
+        if(stackHead!=null) {
+            if(stackHead.val== minStackHead.val) minStackHead = minStackHead.next;
+            stackHead  = stackHead.next;
+        } 
+    }
+
+    public int top() {
+        return stackHead ==null ? 0 : stackHead.val;
+    }
+
+    public int getMin() {
+        return minStackHead==null ? 0 : minStackHead.val;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    
+    ListNode(int x) {
+        val = x;
+    }
+}

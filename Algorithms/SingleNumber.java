e4eccc425db12a82f7651216c82c773a9abe21fb
fucktheinterview <jzy0706@vip.163.com>
2014-11-23 17:40:19 -0800

    Update SingleNumber.java

--- Algorithms/SingleNumber.java
+++ Algorithms/SingleNumber.java
@@ -1,11 +1,10 @@
+// exclusive OR
 public class Solution {
     public int singleNumber(int[] A) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        int number = 0;
-        for(int i=0;i<A.length;i++){
-            number = number^A[i];
+        int result = 0;
+        for(Integer a: A) {
+            result ^=a;
         }
-        return number;
+        return result;
     }
 }

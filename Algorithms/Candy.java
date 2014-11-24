ff4812d9a8c4fd6d0c1165f667923203583eea1d
fucktheinterview <jzy0706@vip.163.com>
2014-11-24 09:30:34 -0800

    Update Candy.java

--- Algorithms/Candy.java
+++ Algorithms/Candy.java
@@ -1,3 +1,27 @@
+// my solution
+
+public class Solution {
+    public int candy(int[] ratings) {
+        int result = 0;
+        if(ratings==null) return result;
+        int[] candy = new int[ratings.length];
+        for(int i=0; i< candy.length; i++) {
+            candy[i] = 1;
+        }
+        for(int i=1; i<ratings.length; i++) {
+            if(ratings[i-1]<ratings[i]) candy[i] = candy[i-1] + 1;
+        }
+        for(int j= ratings.length-2; j>=0; j--) {
+            if((ratings[j]> ratings[j+1]) && (candy[j] <= candy[j+1])) candy[j] = candy[j+1] + 1; 
+        }
+        for(Integer candies: candy) {
+            result+= candies;
+        }
+        return result;
+    }
+}
+
+
 //the time complexity is O(n), the space complexity is also O(n)
 
 public class Solution {

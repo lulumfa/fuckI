7897a06b8fb6ad341083ca2d5eb50d0f10ce69a6
fucktheinterview <jzy0706@vip.163.com>
2014-11-24 12:12:45 -0800

    Update GasStation.java

--- Algorithms/GasStation.java
+++ Algorithms/GasStation.java
@@ -1,3 +1,21 @@
+// my solution
+public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if(gas==null || cost==null || gas.length!= cost.length) return -1;
+        int index = 0, total = 0, sum = 0;
+        for(int i=0; i< gas.length; i++) {
+            sum+=gas[i]-cost[i];
+            if(sum<0){
+                sum=0;
+                index = i+1;
+            }
+            total +=gas[i]-cost[i];
+        }
+        return total>=0 ? index: -1;
+    }
+}
+
+
 // if total>0. there must exists one starting point. separate the loop into positive and negative part to prove
 public class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {

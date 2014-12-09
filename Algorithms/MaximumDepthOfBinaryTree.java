327b2ecbb0fd1e7d1b79a8511ce6f6eb90ea4c41
fucktheinterview <jzy0706@vip.163.com>
2014-12-08 19:05:07 -0800

    Create MaximumDepthOfBinaryTree.java

---
+++ Algorithms/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        TreeNode temp;
+        int curNum = 0;
+        int oldNum =1;
+        int count = 0;
+        while(!queue.isEmpty()) {
+            for(int i = 0; i< oldNum; i++) {
+                temp = queue.poll();
+                if(temp.left!=null) {
+                    curNum++;
+                    queue.offer(temp.left);
+                }
+                if(temp.right!=null) {
+                    curNum++;
+                    queue.offer(temp.right);
+                }
+            }
+            count++;
+            oldNum = curNum;
+            curNum = 0;
+        }
+        return count++;
+    }
+}

0940391c78ad55b33777f1b0b913c8e4cb139c3a
fucktheinterview <jzy0706@vip.163.com>
2014-12-08 16:29:33 -0800

    Update ConstructBinaryTreeFromInorderAndPostorderTraversal.java

--- Algorithms/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ Algorithms/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -1,3 +1,38 @@
+//reference: http://blog.csdn.net/linhuanmars/article/details/24390157
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(inorder==null || postorder==null || inorder.length!=postorder.length) return null;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i = 0; i<inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        
+        return helper(inorder, 0, inorder.length-1, postorder, 0, postorder.length-1, map);
+    }
+    
+    private TreeNode helper(int[] inorder, int inL, int inR, int[] postorder, int postL, int postR, HashMap<Integer, Integer> map) {
+        if(inL>inR || postL > postR) return null;
+        TreeNode root = new TreeNode(postorder[postR]);
+        int index = map.get(root.val);
+        TreeNode left = helper(inorder, inL, index-1, postorder, postL, postL+ index-1-inL, map);
+        TreeNode right = helper(inorder, index+1, inR, postorder, postL + index -inL, postR-1, map);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+}
+
 /**
  * Definition for binary tree
  * public class TreeNode {

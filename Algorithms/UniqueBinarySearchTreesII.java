4c7f25f1976e345fb5880f8a4851e1323bae1d1c
fucktheinterview <jzy0706@vip.163.com>
2014-12-23 08:31:15 +0800

    Update UniqueBinarySearchTreesII.java

--- Algorithms/UniqueBinarySearchTreesII.java
+++ Algorithms/UniqueBinarySearchTreesII.java
@@ -1,3 +1,5 @@
+//reference: http://blog.csdn.net/linhuanmars/article/details/24761437
+
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -8,35 +10,29 @@
  * }
  */
 public class Solution {
-    public ArrayList<TreeNode> generateTrees(int n) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        return generateTrees(1,n);
+    public List<TreeNode> generateTrees(int n) {
+        return helper(1, n);
     }
     
-    public ArrayList<TreeNode> generateTrees(int a, int b){
-        ArrayList<TreeNode> res = new ArrayList<TreeNode>();
-        
-        if(a>b){
-            res.add(null);   
-        }else if(a==b){
-            res.add(new TreeNode(a));
-        }else if(a<b){
-            for(int i=a;i<=b;i++){
-                ArrayList<TreeNode> temp1 = generateTrees(a,i-1);
-                ArrayList<TreeNode> temp2 = generateTrees(i+1,b);
-                
-                for(TreeNode n:temp1){
-                    for(TreeNode m:temp2){
-                        TreeNode temp= new TreeNode(i);
-                        temp.left=n;
-                        temp.right=m;
-                        res.add(temp);
+    private List<TreeNode> helper(int left, int right) {
+        List<TreeNode> res = new ArrayList<TreeNode>();
+        if(left>right) {
+            res.add(null);
+        } else {
+            for(int i = left; i<=right; i++) {
+                List<TreeNode> l = helper(left, i-1);
+                List<TreeNode> r = helper(i+1, right);
+                for(int j = 0; j< l.size(); j++) {
+                    for(int k = 0; k < r.size(); k++) {
+                        TreeNode node = new TreeNode(i);
+                        node.left = l.get(j);
+                        node.right = r.get(k);
+                        res.add(node);
                     }
                 }
-
             }
-        } 
+        }
         return res;
+
     }
 }

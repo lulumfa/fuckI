1a15c1e93757d51f2315e2656b3099d019b74b19
fucktheinterview <jzy0706@vip.163.com>
2015-01-15 12:26:22 -0500

    Create UniquePaths.java

---
+++ Algorithms/UniquePaths.java
@@ -0,0 +1,17 @@
+//reference: http://blog.csdn.net/linhuanmars/article/details/22126357
+
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m<=0 || n<=0) return 0;
+        int[] res = new int[n];
+        for(int i = 0; i<n; i++) {
+            res[i] = 1;
+        }
+        for(int i = 1; i<m; i++) {
+            for(int j =1; j<n; j++) {
+                res[j] = res[j] + res[j-1];
+            }
+        }
+        return res[n-1];
+    }
+}

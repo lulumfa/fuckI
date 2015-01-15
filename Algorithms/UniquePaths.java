431ae73d0994769a572959b201c42bf264bf5e5a
fucktheinterview <jzy0706@vip.163.com>
2015-01-15 14:05:04 -0500

    Update UniquePaths.java

--- Algorithms/UniquePaths.java
+++ Algorithms/UniquePaths.java
@@ -1,6 +1,18 @@
 //reference: http://blog.csdn.net/linhuanmars/article/details/22126357
-// permutation
-
+// permutation, considering the n! could be very large, so using long instead of int to store the res
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m<=0 || n<=0) return 0;
+        int large = Math.max(m-1, n-1);
+        int small = Math.min(m-1, n-1);
+        long mult1 =1, mult2 = 1;
+        for(int i = 1; i <=small; i++) {
+            mult1*= i;
+            mult2*= large+small-i+1;
+        }
+        return (int)(mult2/mult1);
+    }
+}
 
 // NP
 public class Solution {

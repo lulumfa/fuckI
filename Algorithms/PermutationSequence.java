0c10a0672d6b6e1f6b9823b792a7097e6913b6d8
fucktheinterview <jzy0706@vip.163.com>
2015-01-18 11:19:28 -0500

    Update PermutationSequence.java

--- Algorithms/PermutationSequence.java
+++ Algorithms/PermutationSequence.java
@@ -1,3 +1,32 @@
+http://blog.csdn.net/linhuanmars/article/details/22028697
+public class Solution {
+    public String getPermutation(int n, int k) {
+        if(n<=0) return null;
+        StringBuilder res = new StringBuilder();
+        ArrayList<Integer> digits = new ArrayList<Integer>();
+        for(int i=1; i<=n; i++) {
+            digits.add(i);
+        }
+        int fac = 1;
+        for(int i = 2; i<n; i++) {
+            fac*=i;
+        }
+        k--;
+        n--;
+        while(n>=0) {
+            int digit = k/fac;
+            k %=fac;
+            res.append(digits.get(digit));
+            digits.remove(digit);
+            if(n>0) fac/=n;
+            n--;
+        }
+        return res.toString();
+    }
+}
+
+
+// another solution
 public class Solution {
     public String getPermutation(int n, int k) {
         // Start typing your Java solution below

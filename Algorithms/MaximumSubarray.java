6f38699d2c09791e83366470c11e79530b88c5fd
fucktheinterview <jzy0706@vip.163.com>
2015-01-20 14:51:16 -0500

    Update MaximumSubarray.java

--- Algorithms/MaximumSubarray.java
+++ Algorithms/MaximumSubarray.java
@@ -1,3 +1,24 @@
+//reference: http://blog.csdn.net/linhuanmars/article/details/21314059
+
+// 时间上只需要扫描一次数组，所以时间复杂度是O(n)。所以空间复杂度是两个变量（local和global），即O(2)=O(1)。
+
+public class Solution {
+    public int maxSubArray(int[] A) {
+        if(A==null || A.length==0) return Integer.MIN_VALUE;
+        
+        int local = A[0];
+        int global = A[0];
+        
+        for(int i = 1; i <A.length; i++) {
+            local = Math.max(local+A[i], A[i]);
+            global = Math.max(global, local);
+        }
+        return global;
+    }
+}
+
+// 并发现，这道题有两种经典解法，一个是：Kadane算法，算法复杂度O(n)；另外一个是分治法：算法复杂度为O(nlogn)。
+
 public class Solution {
   
     public int maxSum(int[] a, int left, int right){

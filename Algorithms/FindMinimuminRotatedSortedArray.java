8315c2ab6b64293d2f79d224b9ad6c98bf1086b9
fucktheinterview <jzy0706@vip.163.com>
2015-02-02 17:29:23 -0500

    Update FindMinimuminRotatedSortedArray.java

--- Algorithms/FindMinimuminRotatedSortedArray.java
+++ Algorithms/FindMinimuminRotatedSortedArray.java
@@ -1,16 +1,21 @@
+//reference: http://blog.csdn.net/linhuanmars/article/details/40449295
+
 public class Solution {
     public int findMin(int[] num) {
-        if(num==null) return 0;
-        return recursionMin(num, 0, num.length-1);
-    }
-    
-    public int recursionMin(int[] num, int startPin, int endPin) {
-        if(startPin==endPin) return num[startPin];
-        int partition = (endPin+startPin)/2;
-        if(Math.min(num[startPin], num[partition]) <= Math.min(num[partition+1], num[endPin])) {
-            return recursionMin(num, startPin, partition);
-        } else {
-            return recursionMin(num, partition+1, endPin);
+        if(num==null || num.length==0) return Integer.MAX_VALUE;
+        int l = 0; 
+        int r = num.length-1;
+        int min = num[0];
+        while(l<=r) {
+            int mid = (l+r)/2;
+            if(num[l]<=num[mid]) {
+                min = Math.min(min, num[l]);
+                l = mid+1;
+            } else {
+                min = Math.min(min, num[mid+1]);
+                r = mid;
+            }
         }
+        return min;
     }
 }

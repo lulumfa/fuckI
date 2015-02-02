6b421096fb21e8e7a233b9250c16eb7c44931f9b
fucktheinterview <jzy0706@vip.163.com>
2015-02-02 11:03:18 -0500

    Update LowestCommonAncester.java

--- Algorithms/LowestCommonAncester.java
+++ Algorithms/LowestCommonAncester.java
@@ -1,5 +1,8 @@
 // reference: http://stackoverflow.com/questions/1484473/how-to-find-the-lowest-common-ancestor-of-two-nodes-in-any-binary-tree
 
+//But keep in mind that if your nodes have parent pointers, a slight variation on his algorithm is possible. For both nodes in question construct a list containing the path from root to the node by starting at the node, and front inserting the parent.
+
+// generally, it will be O(n)
 public static Node LCA(Node root, Node a, Node b){
    Node left=null,right=null;
 

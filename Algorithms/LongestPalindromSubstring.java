2fbb5952b66298c7ae81dfc1150c5f646131c4ea
fucktheinterview <jzy0706@vip.163.com>
2015-02-12 11:26:24 -0500

    Update LongestPalindromSubstring.java

--- Algorithms/LongestPalindromSubstring.java
+++ Algorithms/LongestPalindromSubstring.java
@@ -2,66 +2,31 @@ http://blog.csdn.net/linhuanmars/article/details/20888595 O(n2)
 https://zhuhongcheng.wordpress.com/2009/08/02/a-simple-linear-time-algorithm-for-finding-longest-palindrome-sub-string/ O(n)
 http://stackoverflow.com/questions/10468208/manachers-algorithm-algorithm-to-find-longest-palindrome-substring-in-linear-t
 
+// my own way
 
 public class Solution {
     public String longestPalindrome(String s) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        if(s.length()==1){
-            return s;
-        }
-        char[] input = s.toCharArray();
-        int i = 0;
-        int len=1;
-        int mid = 1;
-        for(;i<input.length-1;i++){
-            //if(input[i]!=input[i+1]){
-                if(len<checkPalindromeOdd(input,i)){
-                    len = checkPalindromeOdd(input,i);
-                    mid = i;
-                }
-           // }else{
-            if(input[i]==input[i+1]){
-                if(len<checkPalindromeEven(input, i, i+1)){
-                    len= checkPalindromeEven(input, i, i+1);
-                    mid = i;
-                }
-                
-           }
-        }
-        if(len%2==1){
-            return s.substring(mid-len/2,mid+len/2+1);
-        }else{
-            return s.substring(mid-len/2+1,mid+len/2+1);
-        }
-        //return String.valueOf(solution);
-    }
-    public int checkPalindromeOdd(char[] input,int cursor){
-        int l = cursor-1;
-        int u=cursor+1;
-        int len=1;
-        while(l>=0&&u<input.length){
-            if(input[l]!=input[u]){
-                return len;
+        if(s==null||s.length()==0) return s;
+        String res = "";
+        for(int i = 0; i<s.length(); i++) {
+            String one = findLongestP(i, i, s);
+            String localMax = one;
+            if(i+1<s.length() && s.charAt(i)==s.charAt(i+1)) {
+                String two = findLongestP(i, i+1, s);
+                localMax = one.length()>two.length() ? one : two;
             }
-            len+=2;
-            l--;
-            u++;
+            res = localMax.length() > res.length() ? localMax : res;
         }
-        return len;
+        return res;
     }
-    public int checkPalindromeEven(char[] input, int cursor1, int cursor2){
-        int l = cursor1-1;
-        int u=cursor2+1;
-        int len = 2;
-        while(l>=0&&u<input.length){
-            if(input[l]!=input[u]){
-                return len;
-            }
-            len+=2;
-            l--;
-            u++;
+    
+    private String findLongestP(int left, int right, String s) {
+        if(s==null || right>=s.length()) return null;
+        
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)) {
+            left--;
+            right++;
         }
-        return len;
+        return s.substring(left+1, right);
     }
 }

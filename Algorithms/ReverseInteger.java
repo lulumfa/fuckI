2904f70a562250e02072f31d10326903b27d3299
fucktheinterview <jzy0706@vip.163.com>
2015-02-13 15:15:19 -0500

    Update ReverseInteger.java

--- Algorithms/ReverseInteger.java
+++ Algorithms/ReverseInteger.java
@@ -1,19 +1,17 @@
-package leetcode;
-
-public class ReverseInteger {
+public class Solution {
     public int reverse(int x) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-      
-        int pos = Math.abs(x);
-        int left=0;;
-        int result=0;
-        while(pos!=0){
-            left = pos%10;
-            result=result*10+left;
-            pos = pos/10;
+        if(x==0) return 0;
+        long res = 0;
+        int sign = x/Math.abs(x);
+        x= Math.abs(x);
+        long num  = x;
+        while(num!=0) {
+            long temp = num%10;
+            res = res*10+temp;
+            num= num/10;
         }
-        if(x<0) return -result;
-        return result;
+        res = res*sign;
+        if(res>Integer.MAX_VALUE || res<Integer.MIN_VALUE) return 0;
+        return (int)res;
     }
 }

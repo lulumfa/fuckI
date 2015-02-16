b34dba1448137715317079129589d8d0219421c3
fucktheinterview <jzy0706@vip.163.com>
2015-02-16 13:33:24 -0500

    Update Triangle.java

--- Algorithms/Triangle.java
+++ Algorithms/Triangle.java
@@ -15,3 +15,25 @@ public class Solution {
         return res[0];
     }
 }
+
+//时间复杂度是O(n^2)。而空间上每次只需维护一层即可（因为当前层只用到上一层的元素），所以空间复杂度是O(n)
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle==null || triangle.size()==0 || triangle.get(0).size()==0) return 0;
+        int n = triangle.size();
+        int[] sum = new int[n]; 
+        sum[0] = triangle.get(0).get(0);
+        for(int i = 1; i<n; i++) {
+            sum[i] = sum[i-1] + triangle.get(i).get(i);
+            for(int j = i-1; j>=1; j--) {
+                sum[j] =( sum[j]<sum[j-1] ? sum[j] : sum[j-1]) + triangle.get(i).get(j);
+            }
+            sum[0] = sum[0] + triangle.get(i).get(0);
+        }
+        int min = sum[0];
+        for(Integer num : sum) {
+            min = Math.min(min, num);
+        }
+        return min;
+    }
+}

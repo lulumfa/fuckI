1443952fa9d9651fc5100f36cb627dfae1dfa611
fucktheinterview <jzy0706@vip.163.com>
2015-02-15 21:30:44 -0500

    Update ValidPalindrome.java

--- Algorithms/ValidPalindrome.java
+++ Algorithms/ValidPalindrome.java
@@ -1,62 +1,24 @@
 public class Solution {
     public boolean isPalindrome(String s) {
         if(s==null) return false;
-        if(s.length()<=1) return true;
-        int start  = 0;
-        int end = s.length()-1;
+        int left = 0;
+        int right = s.length()-1;
         s = s.toLowerCase();
-        while(start<end) {
-            while(!isAlphanumeric(s.charAt(start))) {
-                start++;
-                if(start>=end) return true;
+        while(left<right) {
+            while(!ifAlphaNumeric(s.charAt(left))) {
+                left++;
+                if(left>=right) return true;
             }
-            while(!isAlphanumeric(s.charAt(end))) {
-                end--;
-                if(start>=end) return true;
+            while(!ifAlphaNumeric(s.charAt(right))) {
+                right--;
+                if(left>=right) return true;
             }
-            if(s.charAt(start) != s.charAt(end)) return false;
-            start++;
-            end--;
+            if(s.charAt(left++)!=s.charAt(right--)) return false;
         }
         return true;
     }
-    private boolean isAlphanumeric(char c) {
-        return (c>='a' && c<= 'z') || (c>='0' && c<= '9');
-    }
-}
-
-class Solution 
-{
-
-//alphanumeric characters  means numerical and alphabetical
-    public boolean isPalindrome(String s) 
-    {
-        if(s==null) return false;
-        if(s.length()<=1) return true;
-        int i=0;
-        
-        // toLowerCase(0 only work on alphatical characters, no worry about others
-        s = s.toLowerCase();
-        int j = s.length()-1;
-        while(i<=j)
-        {
-            if(!checkCharacter(s.charAt(i))) 
-            {
-              i++;
-              continue;
-            }
-            if(!checkCharacter(s.charAt(j))) 
-            {
-              j--;
-              continue;
-            }
-            if(s.charAt(i)!=s.charAt(j)) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    public boolean checkCharacter(char s){
-        return ((s>='a' && s <= 'z')||(s>='0' && s <= '9'));
+    
+    private boolean ifAlphaNumeric(char c) {
+        return (c>='a' && c<='z') || (c>='0' && c<='9');
     }
 }

414a3d96f50dee968313badf98fbde9e42e7a32e
fucktheinterview <jzy0706@vip.163.com>
2015-02-17 18:27:14 -0500

    Update PathSumII.java

--- Algorithms/PathSumII.java
+++ Algorithms/PathSumII.java
@@ -1,3 +1,50 @@
+// my iteratively way
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root==null) return res;
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode pre = null;
+        TreeNode node = root;
+        TreeNode temp;
+        int count = 0;
+        List<Integer> list = new ArrayList<Integer>();
+        while(node!=null || !stack.isEmpty()) {
+            if(node!=null) {
+                list.add(node.val);
+                count+=node.val;
+                stack.push(node);
+                node = node.left;
+            } else {
+                temp = stack.peek();
+                if(temp.right!=null && temp.right!=pre) {
+                    node = temp.right;
+                } else {
+                    if(temp.right==null && temp.left ==null && count==sum) {
+                        ArrayList<Integer> copy = new ArrayList<Integer>(list);
+                        res.add(copy);
+                    }
+                    pre = stack.pop();
+                    list.remove(list.size()-1);
+                    count-=pre.val;
+                }
+            }
+        }
+        return res;
+    }
+}
+
 /**
  * Definition for binary tree
  * public class TreeNode {

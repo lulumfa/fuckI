38cd582b329ba0ef8d7fd8cde6a1d3017dabe094
fucktheinterview <jzy0706@vip.163.com>
2015-02-22 19:23:07 -0500

    Update UniqueBinarySearchTrees.java

--- Algorithms/UniqueBinarySearchTrees.java
+++ Algorithms/UniqueBinarySearchTrees.java
@@ -1,3 +1,22 @@
+// http://blog.csdn.net/linhuanmars/article/details/24761459
+
+// 所以总时间复杂度是O(1+2+...+n)=O(n^2)。空间上需要一个数组来维护，并且需要前i个的所有信息，所以是O(n)。
+
+public class Solution {
+    public int numTrees(int n) {
+        if(n<1) return 0;
+        if(n==1) return 1;
+        int[] res = new int[n+1];
+        res[0] = 1;
+        res[1] = 1;
+        for(int i = 2; i<=n; i++) {
+            for(int j = 0; j<i; j++) {
+                res[i] += res[j] * res[i-j-1];
+            }
+        }
+        return res[n];
+    }
+}
 // Note 1. 第一种方法，用的DP。 第二种方法，考虑f(n) 和 f(n-1) 的关系， 很直接。
 // Link : 阿三哥有个视频链接，讲这题，讲的很清楚，http://www.youtube.com/watch?v=UfA_v0VmiDg
 //           不得不说，阿三哥做视频有一套，经常看youtube的视频都是三哥做的，讲的很清楚，印度英语差点就是了。

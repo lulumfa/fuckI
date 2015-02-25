9412ef549bb45a3f8617e8734a337897bd59360d
fucktheinterview <jzy0706@vip.163.com>
2015-02-25 13:13:22 -0500

    Update RemoveDuplicatesSortedArrayII.java

--- Algorithms/RemoveDuplicatesSortedArrayII.java
+++ Algorithms/RemoveDuplicatesSortedArrayII.java
@@ -3,6 +3,24 @@
 public class Solution {
     public int removeDuplicates(int[] A) {
         if(A==null || A.length==0) return 0;
+        int pre = 0;
+        int dup = 1;
+        for(int i = 1; i<A.length; i++) {
+            if(A[pre]!=A[i]) {
+                A[++pre] = A[i];
+                dup =1;
+            } else if(dup<2){
+                dup++;
+                A[++pre] = A[i];
+            }
+        }
+        return pre+1;
+    }
+}
+
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        if(A==null || A.length==0) return 0;
         
         int index = 1;
         int count = 1;

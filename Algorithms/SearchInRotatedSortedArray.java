752a5e136a9cf714e449c077f306e66bd3c204a3
fucktheinterview <jzy0706@vip.163.com>
2015-02-24 22:21:44 -0500

    Update SearchInRotatedSortedArray.java

--- Algorithms/SearchInRotatedSortedArray.java
+++ Algorithms/SearchInRotatedSortedArray.java
@@ -1,7 +1,7 @@
 // reference: http://blog.csdn.net/linhuanmars/article/details/20525681
 
 // 这里是这样的，因为mid有可能会等于left所以有可能会跳过第一个判断，所以如果要把left放前面，那就需要用>=就可以了哈～ if(A[l] <= A[m]) {
-
+//每次我们都可以切掉一半的数据，所以算法的时间复杂度是O(logn)，空间复杂度是O(1)
 
 public class Solution {
     public int search(int[] A, int target) {

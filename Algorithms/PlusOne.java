16a70e5306171b01990b9d7d03732c8cd9204636
fucktheinterview <jzy0706@vip.163.com>
2015-02-26 14:24:05 -0500

    Update PlusOne.java

--- Algorithms/PlusOne.java
+++ Algorithms/PlusOne.java
@@ -1,5 +1,8 @@
 //reference: http://blog.csdn.net/linhuanmars/article/details/22365957
 
+// my better one
+
+
 public class Solution {
     public int[] plusOne(int[] digits) {
         if(digits==null || digits.length==0) return digits;
@@ -14,6 +17,23 @@ public class Solution {
         }
         int[] res = new int[digits.length+1];
         res[0] =1 ;
+        return res;
+    }
+}
+
+// my general one
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        if(digits==null || digits.length==0) return digits;
+        int carry = 1;
+        for(int i = digits.length-1; i>=0; i--) {
+            int cur = digits[i]+carry;
+            carry = (cur)/10;
+            digits[i] = (cur)%10;
+        }
+        if(carry==0) return digits;
+        int[] res = new int[digits.length+1];
+        res[0] =1;
         return res;
     }
 }

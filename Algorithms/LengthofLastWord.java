270268ad20f3e995932195d688d575beaebe1b27
fucktheinterview <jzy0706@vip.163.com>
2015-02-28 19:13:52 -0500

    Update LengthofLastWord.java

--- Algorithms/LengthofLastWord.java
+++ Algorithms/LengthofLastWord.java
@@ -2,6 +2,24 @@
 
 // 时间复杂度是O(n)，n是字符串的长度，空间复杂度是O(1)
 
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        if(s==null) return 0;
+        int start = -1;
+        int end  = -1;
+        for(int i = s.length()-1; i>=0; i--) {
+            if(s.charAt(i)==' ' && end!=-1) {
+                start = i;
+                break;
+            } else if(s.charAt(i) !=' ' && end==-1) {
+                end = i;
+            }
+        }
+        return end-start;
+    }
+}
+
 public class Solution {
     public int lengthOfLastWord(String s) {
         if(s==null || s.length()==0) return 0;

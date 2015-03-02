18032f68e1df938441e2e68e89857a49b09ad268
fucktheinterview <jzy0706@vip.163.com>
2015-03-02 17:31:21 -0500

    Update LengthOfLongestSubstringWithoutRepeating.java

--- Algorithms/LengthOfLongestSubstringWithoutRepeating.java
+++ Algorithms/LengthOfLongestSubstringWithoutRepeating.java
@@ -1,3 +1,37 @@
+//http://blog.csdn.net/linhuanmars/article/details/19949159
+//因此时间复杂度为O(2*n)=O(n),是线性算法。空间复杂度为HashSet的size,也是O(n). 
+public int lengthOfLongestSubstring(String s) {
+    if(s==null || s.length()==0)
+        return 0;
+    HashSet<Character> set = new HashSet<Character>();
+    int max = 0;
+    int walker = 0;
+    int runner = 0;
+    while(runner<s.length())
+    {
+        if(set.contains(s.charAt(runner)))
+        {
+            if(max<runner-walker)
+            {
+                max = runner-walker;
+            }
+            while(s.charAt(walker)!=s.charAt(runner))
+            {
+                set.remove(s.charAt(walker));
+                walker++;
+            }
+            walker++;
+        }
+        else
+        {
+            set.add(s.charAt(runner));
+        }
+        runner++;
+    }
+    max = Math.max(max,runner-walker);
+    return max;
+}
+
 package leetcode;
 
 

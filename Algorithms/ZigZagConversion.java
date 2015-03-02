c0474cc3e8e5a298dd88af5066a32f398f74556f
fucktheinterview <jzy0706@vip.163.com>
2015-03-02 17:41:53 -0500

    Update ZigZagConversion.java

--- Algorithms/ZigZagConversion.java
+++ Algorithms/ZigZagConversion.java
@@ -1,3 +1,26 @@
+//http://blog.csdn.net/linhuanmars/article/details/21145039
+
+//时间复杂度是O(n),空间复杂度是O(1),代码如下
+public String convert(String s, int nRows) {
+    if(s == null || s.length()==0 || nRows <=0)
+        return "";
+    if(nRows == 1)
+        return s;
+    StringBuilder res = new StringBuilder();
+    int size = 2*nRows-2;
+    for(int i=0;i<nRows;i++)
+    {
+        for(int j=i;j<s.length();j+=size)
+        {
+            res.append(s.charAt(j));
+            if(i!=0 && i!=nRows-1 && j+size-2*i<s.length())
+                res.append(s.charAt(j+size-2*i));
+        }                
+    }
+    return res.toString();
+}
+
+
 public class ZigZagConversion {
     public String convert(String s, int nRows) {
             if (nRows <= 1 || s.length() < 2) return s;

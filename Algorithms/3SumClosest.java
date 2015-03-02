84c405d62e827d95df6c901a1aa27b8e1c492d4d
fucktheinterview <jzy0706@vip.163.com>
2015-03-02 17:20:02 -0500

    Update 3SumClosest.java

--- Algorithms/3SumClosest.java
+++ Algorithms/3SumClosest.java
@@ -28,3 +28,42 @@ public class Solution {
 		return result;
 	}
 }
+
+// reference: http://blog.csdn.net/linhuanmars/article/details/19712011
+
+public int threeSumClosest(int[] num, int target) {
+    if(num == null || num.length<=2)
+        return Integer.MIN_VALUE;
+    Arrays.sort(num);
+    int closest = num[0]+num[1]+num[2]-target;    
+    for(int i=0;i<num.length-2;i++)
+    {
+        int cur = twoSum(num,target-num[i],i+1);
+        if(Math.abs(cur)<Math.abs(closest))
+            closest = cur; 
+    }
+    return target+closest;
+}
+private int twoSum(int[] num, int target, int start)
+{
+    int closest = num[start]+num[start+1]-target;
+    int l = start;
+    int r = num.length-1;
+    while(l<r)
+    {
+        if(num[l]+num[r]==target)
+            return 0;
+        int diff = num[l]+num[r]-target;
+        if(Math.abs(diff)<Math.abs(closest))
+            closest = diff;    
+        if(num[l]+num[r]>target)
+        {
+            r--;
+        }
+        else
+        {
+            l++;
+        }
+    }
+    return closest;
+}

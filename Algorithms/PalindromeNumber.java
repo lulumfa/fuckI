fdec7bcaba51aaaffdfc3a51a49e85fe741a67fd
fucktheinterview <jzy0706@vip.163.com>
2015-03-02 17:48:31 -0500

    Update PalindromeNumber.java

--- Algorithms/PalindromeNumber.java
+++ Algorithms/PalindromeNumber.java
@@ -1,3 +1,20 @@
+//http://blog.csdn.net/linhuanmars/article/details/21145231
+public boolean isPalindrome(int x) {
+    if(x<0)
+        return false;
+    int div = 1;
+    while(div<=x/10)
+        div *= 10;
+    while(x>0)
+    {
+        if(x/div!=x%10)
+            return false;
+        x = (x%div)/10;
+        div /= 100;
+    }
+    return true;
+}
+
 //http://www.programcreek.com/2013/02/leetcode-palindrome-number-java/
 
 public class Solution {

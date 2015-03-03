eee6ba501feb2f3c5a9158587d41c5ea421bad1f
fucktheinterview <jzy0706@vip.163.com>
2015-03-02 23:25:00 -0500

    Update and rename Algorithms to Algorithms/ContainerWithMostWater.java

---
+++ Algorithms/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+//http://blog.csdn.net/linhuanmars/article/details/21145429
+
+public int maxArea(int[] height) {
+    if(height==null || height.length==0)
+        return 0;
+    int l = 0;
+    int r = height.length-1;
+    int maxArea = 0;
+    while(l<r)
+    {
+        maxArea = Math.max(maxArea, Math.min(height[l],height[r])*(r-l));
+        if(height[l]<height[r])
+            l++;
+        else
+            r--;
+    }
+    return maxArea;
+}

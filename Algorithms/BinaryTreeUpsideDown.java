58c9e2020e15e966ea06e847e38dde5db3717011
fucktheinterview <jzy0706@vip.163.com>
2015-03-03 15:34:15 -0500

    Update and rename Algorithms to Algorithms/BinaryTreeUpsideDown.java

---
+++ Algorithms/BinaryTreeUpsideDown.java
@@ -0,0 +1,14 @@
+// http://blog.csdn.net/whuwangyi/article/details/43186045
+
+	public TreeNode UpsideDownBinaryTree(TreeNode root) {
+		TreeNode node = root, parent = null, right = null;
+		while (node != null) {
+			TreeNode left = node.left;
+			node.left = right;
+			right = node.right;
+			node.right = parent;
+			parent = node;
+			node = left;
+		}
+		return parent;
+	}

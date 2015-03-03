c1cc48db1aa5710e9cf6c4c0b2feffb0eaea3f9c
fucktheinterview <jzy0706@vip.163.com>
2015-03-02 23:09:44 -0500

    Update and rename Algorithms to Algorithms/Remove NthNodeFromEndofList.java

---
+++ Algorithms/Remove NthNodeFromEndofList.java
@@ -0,0 +1,25 @@
+//http://blog.csdn.net/linhuanmars/article/details/19778441
+
+public ListNode removeNthFromEnd(ListNode head, int n) {
+    if(head == null)
+        return null;
+    int i=0;
+    ListNode runner = head;
+    while(runner!=null && i<n)
+    {
+        runner = runner.next;
+        i++;
+    }
+    if(i<n)
+        return head;
+    if(runner == null)
+        return head.next;
+    ListNode walker = head;
+    while(runner.next!=null)
+    {
+        walker = walker.next;
+        runner = runner.next;
+    }
+    walker.next = walker.next.next;
+    return head;
+}

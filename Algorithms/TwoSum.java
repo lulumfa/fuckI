4fbfe65710d76b830efd03e7f739a3b65af07ca9
fucktheinterview <jzy0706@vip.163.com>
2015-03-18 00:25:41 -0400

    Update TwoSum.java

--- Algorithms/TwoSum.java
+++ Algorithms/TwoSum.java
@@ -1,36 +1,18 @@
-package leetcode;
-import java.util.*;
-public class TwoSum {
-	
-
-	    public int[] twoSum(int[] numbers, int target) {
-	        // IMPORTANT: Please reset any member data you declared, as
-	        // the same Solution instance will be reused for each test case.
-	        Hashtable<Integer, Integer> check = new Hashtable<Integer, Integer>();
-	        // int max =Integer.MIN_VALUE;
-	        // for(int i =0; i<numbers.length;i++){
-	        //     if(numbers[i]>max) max = numbers[i];
-	        // } 
-	        for(int i = 0; i< numbers.length;i++){
-	            check.put(numbers[i], i);
-	        }
-	        for(int i =0 ; i< numbers.length; i++){
-	            if(check.containsKey(target-numbers[i])){
-	                int index1 = i;
-	                int index2 = (int)check.get(target-numbers[i]);
-	                if(index1>index2){
-	                    int temp = index1;
-	                    index1 = index2;
-	                    index2 = temp;
-	                }
-	                int[] solution = new int[2];
-	                solution[0] = index1+1;
-	                solution[1] = index2+1;
-	                return solution;
-	            }
-	        }
-	        System.out.println("Not found!");
-	        return null;
-	    }
-	
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = {0, 0};
+        if(numbers==null || numbers.length<=1) return res;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i<numbers.length; i++) {
+            if(map.get(target-numbers[i])!=null) {
+                int small = Math.min(i, map.get(target-numbers[i]));
+                int large = Math.max(i, map.get(target-numbers[i]));
+                res[0] = small +1;
+                res[1] = large +1;
+            } else {
+                map.put(numbers[i], i);
+            }
+        }
+        return res;
+    }
 }

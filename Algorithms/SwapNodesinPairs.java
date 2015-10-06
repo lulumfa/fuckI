e4ea03bb59ca4ad7a04dc1c91c82ebba047a4dad
fucktheinterview <jzy0706@vip.163.com>
2015-10-05 22:43:37 -0700

    Update SwapNodesinPairs.java

--- Algorithms/SwapNodesinPairs.java
+++ Algorithms/SwapNodesinPairs.java
@@ -1,5 +1,7 @@
 // http://blog.csdn.net/linhuanmars/article/details/19948569
 
+
+
 public ListNode swapPairs(ListNode head) {
     if(head == null)
         return null;
@@ -20,4 +22,21 @@ public ListNode swapPairs(ListNode head) {
         cur = next;
     }
     return helper.next;
+}
+
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null) return head;
+        ListNode header = new ListNode(0);
+        header.next = head;
+        ListNode node = header;
+        while(node.next!=null && node.next.next!=null){
+            ListNode temp = node.next;
+            node.next = temp.next;
+            temp.next = node.next.next;
+            node.next.next = temp;
+            node = node.next.next;
+        }
+        return header.next;
+    }
 }

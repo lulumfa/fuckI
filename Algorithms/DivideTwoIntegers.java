5e3901a5cb7604bb0b4d203bbffa063203a54036
fucktheinterview <jzy0706@vip.163.com>
2015-10-18 14:10:00 -0700

    Update DivideTwoIntegers.java

--- Algorithms/DivideTwoIntegers.java
+++ Algorithms/DivideTwoIntegers.java
@@ -2,6 +2,39 @@
 
 //所以时间复杂度为O(logn)。
 
+public class Solution {
+    public int divide(int dividend, int divisor) {
+        if(divisor == 0) return Integer.MAX_VALUE;
+        int res = 0;
+        boolean isNeg = (dividend ^ divisor) >>> 31 == 1;
+        if(dividend == Integer.MIN_VALUE){
+            dividend += Math.abs(divisor);
+            if(divisor == -1){
+                return Integer.MAX_VALUE;
+            }
+            res++;
+        }
+        
+        if(divisor == Integer.MIN_VALUE) return res;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        int digit = 0;
+        while(divisor <= (dividend>>1)){
+            divisor<<=1;
+            digit++;
+        }
+        while(digit>=0){
+            if(dividend >= divisor){
+                res += 1<<digit;
+                dividend-= divisor;
+            }
+            divisor >>=1;
+            digit--;
+        }
+        return isNeg ? -res: res;
+    }
+}
+
 package leetcode;
 
 public class DivideTwoIntegers {

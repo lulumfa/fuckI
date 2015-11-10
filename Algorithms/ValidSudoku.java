8c3e625921965521cf76b98806f131575c2cc6e8
fucktheinterview <jzy0706@vip.163.com>
2015-11-09 21:24:07 -0800

    Update ValidSudoku.java

--- Algorithms/ValidSudoku.java
+++ Algorithms/ValidSudoku.java
@@ -53,3 +53,53 @@ public boolean isValidSudoku(char[][] board) {
     }
     return true;
 }
+
+
+// my own
+
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        if(board == null || board.length != 9  || board[0].length != 9) return false;
+        for(int i = 0; i < 9; i++){
+            boolean[] valid = new boolean[9];
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    if(valid[board[i][j] -'1']) {
+                        return false;
+                    } else {
+                        valid[board[i][j] -'1'] = true;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < 9; i++){
+            boolean[] valid = new boolean[9];
+            for(int j = 0; j < 9; j++){
+                if(board[j][i] != '.'){
+                    if(valid[board[j][i] -'1']){
+                        return false;
+                    } else {
+                        valid[board[j][i] -'1'] = true;
+                    }
+                }
+            }
+        }
+        
+        for(int block = 0 ; block < 9; block++){
+            boolean[] valid = new boolean[9];
+            for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 3; j++){
+                    if(board[block/3*3 + i][block%3*3 + j] != '.'){
+                        if(valid[board[block/3*3 + i][block%3*3 + j] -'1']) {
+                            return false;
+                        } else {
+                            valid[board[block/3*3 + i][block%3*3 + j] -'1'] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+        
+    }
+}

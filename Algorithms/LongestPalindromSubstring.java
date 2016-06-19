6d189dabddc5f45ad4ef19eb5bb8d40cfdd3e618
fucktheinterview <jzy0706@vip.163.com>
2016-06-19 02:30:10 -0700

    Update LongestPalindromSubstring.java

--- Algorithms/LongestPalindromSubstring.java
+++ Algorithms/LongestPalindromSubstring.java
@@ -1,3 +1,43 @@
+public class Solution {
+public String longestPalindrome(String s) {
+	if (s.isEmpty()) {
+		return null;
+	}
+ 
+	if (s.length() == 1) {
+		return s;
+	}
+ 
+	String longest = s.substring(0, 1);
+	for (int i = 0; i < s.length(); i++) {
+		// get longest palindrome with center of i
+		String tmp = helper(s, i, i);
+		if (tmp.length() > longest.length()) {
+			longest = tmp;
+		}
+ 
+		// get longest palindrome with center of i, i+1
+		tmp = helper(s, i, i + 1);
+		if (tmp.length() > longest.length()) {
+			longest = tmp;
+		}
+	}
+ 
+	return longest;
+}
+ 
+// Given a center, either one letter or two letter, 
+// Find longest palindrome
+public String helper(String s, int begin, int end) {
+	while (begin >= 0 && end <= s.length() - 1 && s.charAt(begin) == s.charAt(end)) {
+		begin--;
+		end++;
+	}
+	return s.substring(begin + 1, end);
+}
+}
+
+
 http://blog.csdn.net/linhuanmars/article/details/20888595 O(n2)
 https://zhuhongcheng.wordpress.com/2009/08/02/a-simple-linear-time-algorithm-for-finding-longest-palindrome-sub-string/ O(n)
 http://stackoverflow.com/questions/10468208/manachers-algorithm-algorithm-to-find-longest-palindrome-substring-in-linear-t

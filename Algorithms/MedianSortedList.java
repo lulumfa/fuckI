83ffc5731a9ef1c21d6cd4cbd2b30dd8eb04edb4
fucktheinterview <jzy0706@vip.163.com>
2016-06-18 19:05:57 -0700

    Update MedianSortedList.java

--- Algorithms/MedianSortedList.java
+++ Algorithms/MedianSortedList.java
@@ -1,3 +1,36 @@
+// latest mine
+
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1 == null && nums2 ==null || nums1.length ==0 && nums2.length ==0 ) return 0;
+        if(nums1 == null || nums1.length ==0) return nums2.length %2 == 0 ? (nums2[nums2.length/2-1] + nums2[nums2.length/2])/2.0d : nums2[nums2.length/2];
+        if(nums2 == null || nums2.length ==0) return nums1.length %2 == 0 ? (nums1[nums1.length/2-1] + nums1[nums1.length/2])/2.0d : nums1[nums1.length/2];
+        int len = nums1.length + nums2.length;
+        int k= len /2;
+        return len%2 ==0 ? (findMedian(nums1, 0, nums1.length-1, nums2, 0, nums2.length-1, k) + findMedian(nums1, 0, nums1.length-1, nums2, 0, nums2.length-1, k+1))/2.0d : 
+            findMedian(nums1, 0, nums1.length-1, nums2, 0, nums2.length-1, k+1);
+    }
+    
+    private double findMedian(int[] nums1, int i1, int i2, int[] nums2, int j1, int j2, int k){
+
+        int len1 = i2 - i1 + 1;
+        int len2 = j2 - j1 + 1;
+        if(len1 > len2) return findMedian(nums2, j1, j2, nums1, i1, i2, k);
+
+        if(len1 <= 0) return nums2[j1 + k -1];
+        if(k==1) return Math.min(nums1[i1], nums2[j1]);
+
+        int pos1 = Math.min(len1, k/2);
+        int pos2 = k- pos1;
+        int val1 = nums1[i1 + pos1 -1];
+        int val2 = nums2[j1 + pos2 -1];
+        if(val1 == val2) return val1;
+        else if(val1 < val2){
+            return this.findMedian(nums1, i1 + pos1, i2, nums2, j1, j1 + pos2-1, k-pos1);
+        }else {
+            return this.findMedian(nums1, i1, i1 + pos1 -1, nums2, j1 + pos2, j2, k-pos2);
+        }
+    }
+
 // reference: http://blog.csdn.net/linhuanmars/article/details/19905515
 
 public double findMedianSortedArrays(int A[], int B[]) {

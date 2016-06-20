39e9785a5b66c5a585a5d47c5fa671cf46dae8f0
fucktheinterview <jzy0706@vip.163.com>
2016-06-19 21:51:23 -0700

    Update ReverseInteger.java

--- Algorithms/ReverseInteger.java
+++ Algorithms/ReverseInteger.java
@@ -1,3 +1,30 @@
+// char[] ever slower
+public class Solution {
+    public int reverse(int x) {
+        if(x == 0) return x;
+        int flag = x > 0 ? 1: -1;
+        Long longX = new Long(x);
+        longX = Math.abs(longX);
+        
+        char[] list = String.valueOf(longX).toCharArray();
+        int left = 0;
+        int right =list.length -1;
+        while(left < right){
+            char temp = list[left];
+            list[left] = list[right];
+            list[right] = temp;
+            left++;
+            right--;
+        }
+        
+        long res = Long.parseLong(String.valueOf(list));
+
+        res = res * flag;
+        if(res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) return 0;
+        return (int)res;
+    }
+}
+
 public class Solution 
 {
     public int reverse(int x) 

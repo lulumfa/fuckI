7631f5abf70b6263680c6b58591814f47dfe21eb
fucktheinterview <jzy0706@vip.163.com>
2016-06-22 23:08:05 -0700

    Update IntegertoRoman.java

--- Algorithms/IntegertoRoman.java
+++ Algorithms/IntegertoRoman.java
@@ -1,3 +1,49 @@
+// my own
+
+public class Solution {
+    public String intToRoman(int num) {
+        
+        // I V X L C D M
+        StringBuilder sb = new StringBuilder();
+        if(num < 0 || num > 3999) return sb.toString();
+        
+        convert(sb, num/1000, 'M', '-', '-');
+        convert(sb, num/100%10, 'C', 'D', 'M');
+        convert(sb, num/10%10, 'X', 'L', 'C');
+        convert(sb, num%10, 'I', 'V', 'X');
+        return sb.toString();
+    }
+    
+    private void convert(StringBuilder sb, int digit, char one, char five, char ten){
+        switch(digit){
+            case 9: 
+                sb.append(one);
+                sb.append(ten);
+                break;
+            case 8:
+            case 7:
+            case 6:
+            case 5:
+                sb.append(five);
+                for(int i = digit; i >5; i--){
+                    sb.append(one);
+                }
+                break;
+            case 4:
+                sb.append(one);
+                sb.append(five);
+                break;      
+            case 3:
+            case 2:
+            case 1:
+                for(int i = digit; i >0; i--){
+                    sb.append(one);
+                }
+                break;
+        }
+    }
+}
+
 // http://blog.csdn.net/linhuanmars/article/details/21145639
 
 public String intToRoman(int num) {

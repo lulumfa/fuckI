5e1c27395e40d32d9db0fe657e4aae3a2e260781
fucktheinterview <jzy0706@vip.163.com>
2016-06-29 23:29:55 -0700

    Create GroupAnagram.java

---
+++ Algorithms/GroupAnagram.java
@@ -0,0 +1,28 @@
+//http://www.programcreek.com/2014/04/leetcode-anagrams-java/
+
+public List<List<String>> groupAnagrams(String[] strs) {
+    List<List<String>> result = new ArrayList<List<String>>();
+ 
+    HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+    for(String str: strs){
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        String ns = new String(arr);
+ 
+        if(map.containsKey(ns)){
+            map.get(ns).add(str);
+        }else{
+            ArrayList<String> al = new ArrayList<String>();
+            al.add(str);
+            map.put(ns, al);
+        }
+    }
+ 
+    for(Map.Entry<String, ArrayList<String>> entry: map.entrySet()){
+        Collections.sort(entry.getValue());
+    }
+ 
+    result.addAll(map.values());
+ 
+    return result;
+}

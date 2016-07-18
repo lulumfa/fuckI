5dc271750c9eaf1212427421335752b9a451ef86
fucktheinterview <jzy0706@vip.163.com>
2016-07-17 19:18:33 -0700

    Update ValidSudoku.java

--- Algorithms/ValidSudoku.java
+++ Algorithms/ValidSudoku.java
@@ -1,3 +1,40 @@
+// my own 
+
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        if(board == null || board.length != 9 || board[0].length !=9) return false;
+        for(int i = 0; i< 9; i++){
+            boolean[] digits = new boolean[9];
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.' && digits[board[i][j] - '1']) return false;
+                else if(board[i][j] != '.') digits[board[i][j]- '1'] = true;
+            }
+        }
+
+        for(int i = 0; i< 9; i++){
+            boolean[] digits = new boolean[9];
+            for(int j = 0; j < 9; j++){
+                if(board[j][i] != '.' && digits[board[j][i]- '1']) return false;
+                else if(board[j][i] != '.') digits[board[j][i]- '1'] = true;
+            }
+        }
+        
+        for(int i = 0; i< 9; i++){
+            int x = i/3 *3;
+            int y = i%3 * 3;
+            boolean[] digits = new boolean[9];
+
+            for(int m = 0; m< 3; m++){
+                for(int n =0; n < 3; n++){
+                    if(board[x+m][y+n] != '.' && digits[board[x+m][y+n]- '1']) return false;
+                    else if(board[x+m][y+n] != '.') digits[board[x+m][y+n]- '1'] = true;
+                }
+            }
+        }
+        return true;
+    }
+}
+
 // http://blog.csdn.net/linhuanmars/article/details/20748171
 
 public boolean isValidSudoku(char[][] board) {

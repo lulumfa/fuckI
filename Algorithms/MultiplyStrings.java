9ce4274e0845e59df45bb3a6dd8d67a9e9085c22
fucktheinterview <jzy0706@vip.163.com>
2016-07-24 21:52:34 -0700

    Update MultiplyStrings.java

--- Algorithms/MultiplyStrings.java
+++ Algorithms/MultiplyStrings.java
@@ -1,3 +1,27 @@
+// my own (m+n)^2, space constant
+
+public class Solution {
+    public String multiply(String num1, String num2) {
+        if(num1 == null || num2 == null || num1.length() == 0 || num2.length() ==0) return "";
+        if(num1.equals("0") || num2.equals("0")) return "0";
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
+        for(int i = num1.length() + num2.length(); i >0; i--){
+            for(int j = Math.min(i-1, num1.length()); j>0; j--){
+                if((i-j) <= num2.length()){
+                    num+= (int)(num1.charAt(j-1) - '0') * (int)(num2.charAt(i-j-1) -'0');
+                }
+            }
+            if(i!=1 || num >0){
+                sb.insert(0, num%10);
+            }
+            num /= 10;
+        }
+        
+        return sb.toString();
+    }
+}
+
 // http://blog.csdn.net/linhuanmars/article/details/20967763
 
 public String multiply(String num1, String num2) {

5878afc1024c4e4997296f3717680fc0e9f3844a
fucktheinterview <jzy0706@vip.163.com>
2016-07-27 12:54:15 -0700

    Update Pow(x,n).java

--- Algorithms/Pow(x,n).java
+++ Algorithms/Pow(x,n).java
@@ -1,3 +1,45 @@
+// more robust
+
+//http://blog.csdn.net/linhuanmars/article/details/20092829
+// bit way
+public double pow(double x, int n) {  
+    if(n==0)  
+        return 1.0;  
+    double res = 1.0;     
+    if(n<0)  
+    {  
+        if(x>=1.0/Double.MAX_VALUE||x<=1.0/-Double.MAX_VALUE)  
+            x = 1.0/x;  
+        else  
+            return Double.MAX_VALUE;  
+        if(n==Integer.MIN_VALUE)  
+        {  
+            res *= x;  
+            n++;  
+        }  
+    }  
+    n = Math.abs(n);  
+    boolean isNeg = false;  
+    if(n%2==1 && x<0)  
+    {  
+        isNeg = true;  
+    }  
+    x = Math.abs(x);  
+    while(n>0)  
+    {  
+        if((n&1) == 1)  
+        {  
+            if(res>Double.MAX_VALUE/x)  
+                return Double.MAX_VALUE;  
+            res *= x;  
+        }  
+        x *= x;  
+        n = n>>1;  
+    }  
+    return isNeg?-res:res;  
+}  
+
+
 public class Solution {
     public double pow(double x, int n) {
         // Start typing your Java solution below

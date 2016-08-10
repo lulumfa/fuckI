dfd2e58d4e9c8ca5164d042355f83719b1cf0900
fucktheinterview <jzy0706@vip.163.com>
2016-08-09 21:31:19 -0700

    Update MergeIntervals.java

--- Algorithms/MergeIntervals.java
+++ Algorithms/MergeIntervals.java
@@ -1,3 +1,29 @@
+// faster though still nlogn
+
+public class Solution {
+public List<Interval> merge(List<Interval> intervals) {
+	// sort start&end
+	int n = intervals.size();
+	int[] starts = new int[n];
+	int[] ends = new int[n];
+	for (int i = 0; i < n; i++) {
+		starts[i] = intervals.get(i).start;
+		ends[i] = intervals.get(i).end;
+	}
+	Arrays.sort(starts);
+	Arrays.sort(ends);
+	// loop through
+	List<Interval> res = new ArrayList<Interval>();
+	for (int i = 0, j = 0; i < n; i++) { // j is start of interval.
+		if (i == n - 1 || starts[i + 1] > ends[i]) {
+			res.add(new Interval(starts[j], ends[i]));
+			j = i + 1;
+		}
+	}
+	return res;
+}
+}
+
 // reference: http://blog.csdn.net/linhuanmars/article/details/21857617
 
 // 整个算法是先排序，然后再做一次线性遍历，时间复杂度是O(nlogn+n)=O(nlogn)，空间复杂度是O(1)，因为不需要额外空间，只有结果集的空间

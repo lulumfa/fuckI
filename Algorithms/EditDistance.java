2a3e507af6c81e92eaad19d5abfec895d62e6925
fucktheinterview <jzy0706@vip.163.com>
2016-08-18 00:43:24 -0700

    Update EditDistance.java

--- Algorithms/EditDistance.java
+++ Algorithms/EditDistance.java
@@ -1,3 +1,38 @@
+// my latest one
+public class Solution {
+public int minDistance(String word1, String word2) {
+	        if(word1 == null && word2 == null) return 0;
+	        if(word1 == null || word1.length() ==0) return word2.length();
+	        if(word2 == null || word2.length() ==0) return word1.length();
+	        
+	        String small = word1.length() > word2.length() ? word2 : word1;
+	        String large = word1.length() > word2.length() ? word1 : word2;
+	        
+	        int lenS = small.length();
+	        int lenL = large.length();
+	        
+	        int[] dp = new int[lenS +1];
+	        
+	        for(int i = 0; i<= lenS; i++){
+	            dp[i] = i;
+	        }
+	        
+	        for(int i = 1; i<= lenL; i++){
+	            int[] newDp = new int[lenS +1];
+	            newDp[0] = i;
+	            for(int j = 1; j<= lenS; j++){
+	                if(small.charAt(j -1) == large.charAt(i -1)){
+	                    newDp[j] = dp[j-1];
+	                } else {
+	                    newDp[j] = Math.min(Math.min(newDp[j-1], dp[j]), dp[j-1]) +1;
+	                }
+	            }
+	            dp = newDp;
+	        }
+	        return dp[lenS];
+	    }
+}
+
 //reference: http://blog.csdn.net/linhuanmars/article/details/24213795
 
 public class Solution {

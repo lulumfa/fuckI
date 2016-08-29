4c20eb6aea94ea46d5db60d1a7edaf9d7457889d
fucktheinterview <jzy0706@vip.163.com>
2016-08-28 23:56:19 -0700

    Update Combinations.java

--- Algorithms/Combinations.java
+++ Algorithms/Combinations.java
@@ -1,3 +1,31 @@
+// iteratively
+
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        if(k > n || n < 1) return res;
+        List<List<Integer>> temp = new ArrayList<List<Integer>>();
+        temp.add(new ArrayList<Integer>());
+        
+        for(int i = 1; i<=n; i++){
+            List<List<Integer>> tempCopy = new ArrayList<List<Integer>>(temp);
+            for(int j = 0; j< temp.size(); j++){
+                List<Integer> newList = new ArrayList<Integer>(temp.get(j));
+                newList.add(i);
+                if(newList.size() == k){
+                    res.add(newList);
+                } else if((k- newList.size()) <= (n-i)) {
+                    tempCopy.add(newList);
+                }
+            }
+            temp = tempCopy;
+        }
+        return res;
+    }
+}
+
+
 // reference: http://blog.csdn.net/linhuanmars/article/details/21260217
 
 public class Solution {

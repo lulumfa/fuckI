271d1c7d1c7501ef8db2b268c3f2bf61aacd714f
fucktheinterview <jzy0706@vip.163.com>
2016-09-09 23:40:27 -0700

    Update WordSearch.java

--- Algorithms/WordSearch.java
+++ Algorithms/WordSearch.java
@@ -1,3 +1,36 @@
+// my own
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        if(board == null || board.length ==0 || board[0].length ==0 || word == null || word.length() ==0) return false;
+        
+        for(int i = 0; i< board.length; i++){
+            for(int j = 0; j< board[0].length; j++){
+                if(dfs(board, i, j, word, 0)) return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean dfs(char[][] board, int i, int j, String word, int k) {
+        if(board[i][j] == word.charAt(k)){
+            if(k == (word.length()-1)) return true;
+            board[i][j] = '0';
+            
+            if(
+                (i>0 && board[i-1][j] != '0' && dfs(board, i-1, j, word, k+1)) ||
+                ((i+1)< board.length && board[i+1][j] != '0' && dfs(board, i+1, j, word, k+1)) ||
+                (j>0 && board[i][j-1] != '0' && dfs(board, i, j -1, word, k+1)) ||
+                ((j+1)< board[0].length && board[i][j+1] != '0' && dfs(board, i, j+1, word, k+1))
+            ) {
+                return true;
+            }
+
+            board[i][j] = word.charAt(k);
+        } 
+        return false;
+    }
+}
+
 // word search II ,http://www.programcreek.com/2014/06/leetcode-word-search-ii-java/
 
 //reference: http://blog.csdn.net/linhuanmars/article/details/24336987

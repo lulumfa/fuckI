b5a86f9d4ab6b137b0fbe9487d7f688498e6f7a4
fucktheinterview <jzy0706@vip.163.com>
2016-09-11 14:24:20 -0700

    Update RemoveDuplicatesSortedArrayII.java

--- Algorithms/RemoveDuplicatesSortedArrayII.java
+++ Algorithms/RemoveDuplicatesSortedArrayII.java
@@ -1,3 +1,28 @@
+// my latest
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null && nums.length ==0) return 0;
+        
+        int res = 1;
+        int count = 1;
+        int k = 2;
+        for(int i =1; i< nums.length; i++){
+            if(nums[i] == nums[i-1]) {
+                count++;
+                if(count <=k) {
+                    res++;
+                }
+            } else {
+                count = 1;
+                res++;
+            }
+            nums[res-1] = nums[i];
+        }
+        
+        return res;
+    }
+}
+
 //reference: http://blog.csdn.net/linhuanmars/article/details/24343525
 
 public class Solution {

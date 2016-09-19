1a880125c7ab3b19295f2b060a906d07dead5eb2
fucktheinterview <jzy0706@vip.163.com>
2016-09-18 17:41:19 -0700

    Update GameOfLife.java

--- Algorithms/GameOfLife.java
+++ Algorithms/GameOfLife.java
@@ -78,3 +78,42 @@ public class Solution {
         } 
     }
 }
+
+
+//my own unwrapper version
+
+public class Solution {
+    public void gameOfLife(int[][] board) {
+        if(board == null || board.length ==0 || board[0].length ==0) return;
+        
+        for(int i =0; i< board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                updateCell(board, i, j);
+            }
+        }
+        
+        for(int i = 0; i< board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                board[i][j] = board[i][j] >>1;
+            }
+        }
+        
+    }
+    
+    private void updateCell(int[][] board, int i, int j) {
+        int m = board.length;
+        int n = board[0].length;
+        int count = ((i > 0 && j >0) ? (board[(i-1 + m)%m][(j- 1 +n)%n] & 1) : 0 )+
+                    ((i > 0) ? (board[(i-1 + m)%m][j] & 1) : 0 ) +
+                    ((i > 0 && (j+1) < n) ? (board[(i-1 + m)%m][(j + 1)%n] & 1 ) : 0 ) +    
+                    ( (j > 0) ? (board[i][(j- 1 +n)%n] & 1) : 0  ) + 
+                    (((j+1) < n) ? (board[i][(j + 1)%n] & 1 ) : 0 ) +  
+                    (((i+1) < m && j >0) ? (board[(i +1)%m][(j - 1 + n)%n] & 1) : 0  )+
+                    ( ((i+1) < m) ? (board[(i + 1)%m][j] & 1 ) : 0 ) +
+                    ( ((i+1) < m && (j+1) < n) ?(board[(i + 1)%m][(j + 1)%n] & 1) : 0 );
+
+        if( ( board[i][j] == 1 && (count == 2 || count ==3) ) || ( board[i][j] == 0 && count == 3) ) {
+            board[i][j] += (1 << 1);
+        } 
+    }
+}

5c0c32ff1e7b25e8fa85e1b0ac3e6f45a8866df0
fucktheinterview <jzy0706@vip.163.com>
2016-09-18 17:34:10 -0700

    Update GameOfLife.java

--- Algorithms/GameOfLife.java
+++ Algorithms/GameOfLife.java
@@ -39,3 +39,42 @@ public int liveNeighbors(int[][] board, int m, int n, int i, int j) {
     return lives;
 }
 }
+
+
+// my version with wrapping boarding
+
+public class Solution {
+    public void gameOfLife(int[][] board) {
+        if(board == null || board.length ==0 || board[0].length ==0) return;
+        
+        for(int i =0; i< board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                updateCell(board, i, j);
+            }
+        }
+        
+        for(int i = 0; i< board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                board[i][j] = board[i][j] >>1;
+            }
+        }
+        
+    }
+    
+    private void updateCell(int[][] board, int i, int j) {
+        int m = board.length;
+        int n = board[0].length;
+        int count = (board[(i-1 + m)%m][(j- 1 +n)%n] & 1) +
+                    (board[(i-1 + m)%m][j] & 1) +
+                    (board[(i-1 + m)%m][(j + 1)%n] & 1 )+    
+                    (board[i][(j- 1 +n)%n] & 1)  + 
+                    (board[i][(j + 1)%n] & 1 ) +  
+                    (board[(i +1)%m][(j - 1 + n)%n] & 1) +
+                    (board[(i + 1)%m][j] & 1 )+
+                    (board[(i + 1)%m][(j + 1)%n] & 1);
+
+        if( ( board[i][j] == 1 && (count == 2 || count ==3) ) || ( board[i][j] == 0 && count == 3) ) {
+            board[i][j] += (1 << 1);
+        } 
+    }
+}

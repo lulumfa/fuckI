ea8fcac5ab8ca53b93da56e49d5f8bac67033ce9
fucktheinterview <jzy0706@vip.163.com>
2016-09-26 00:49:46 -0700

    Update CoinChange.java

--- Algorithms/CoinChange.java
+++ Algorithms/CoinChange.java
@@ -1,3 +1,25 @@
+//np, O(kn) k = coins.length
+// the sequence of coins does not matter, it is actually able to calculate all the combinations.
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(coins == null || amount <1) return 0;
+        
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(Integer coin: coins) {
+            for(int i = coin; i<= amount; i++) {
+                if(dp[i-coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
+
+
+
 package Leet;
  
 import java.util.ArrayList;

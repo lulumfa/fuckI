a0f4c227c6ab0d9d35d9ae517cf71da6d097db82
fucktheinterview <jzy0706@vip.163.com>
2016-09-26 00:31:59 -0700

    Create CombinationSumIV.java

---
+++ Algorithms/CombinationSumIV.java
@@ -0,0 +1,38 @@
+//dp O(mn), space O(n)
+
+// if nums size small, then sort first
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        if(nums == null || target <=0) return 0;
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        Arrays.sort(nums);
+        for(int i = 1; i<= target; i++) {
+            for(int j = 0; j < nums.length && nums[j] <=i; j++) {
+                if(nums[j] <= i) {
+                    dp[i] += dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
+
+// normal one
+
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        if(nums == null || target <=0) return 0;
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        
+        for(int i = 1; i<= target; i++) {
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[j] <= i) {
+                    dp[i] += dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+}

a4c6aa163e1b504261209b85dead73270005c2cf
fucktheinterview <jzy0706@vip.163.com>
2016-09-25 21:31:08 -0700

    Update WildcardMatching.java

--- Algorithms/WildcardMatching.java
+++ Algorithms/WildcardMatching.java
@@ -1,3 +1,36 @@
+// my cleaner way
+// O(n2) space O(m)  m= s.length as the length of the string
+
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        if(s == null || p == null) return false;
+        
+   // for leetcode TLE     
+//if(s.length()>300 && p.charAt(0)=='*' && p.charAt(p.length()-1)=='*')  
+  //  return false; 
+        
+        boolean[] res = new boolean[s.length()+1];
+        res[0] = true;
+        
+        for(int j = 0; j< p.length(); j++) {
+            if(p.charAt(j) != '*') {
+                for(int i = s.length()-1; i>=0; i--) {
+                    res[i+1] = res[i] && (p.charAt(j) == s.charAt(i) || p.charAt(j) == '?');
+                }
+                res[0] = false;
+            } else {
+                int i = 0;
+                while(i <= s.length() && !res[i]) i++;
+                for(; i<= s.length(); i++) {
+                    res[i] = true;
+                }
+            }
+        }
+        
+        return res[s.length()];
+    }
+}
+
 // http://blog.csdn.net/linhuanmars/article/details/21198049
 
 public boolean isMatch(String s, String p) {

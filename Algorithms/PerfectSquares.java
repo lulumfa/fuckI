28affa7bd3d0512ab6930a9284c2e02c56daf3fa
fucktheinterview <jzy0706@vip.163.com>
2016-09-28 19:26:23 -0700

    Update PerfectSquares.java

--- Algorithms/PerfectSquares.java
+++ Algorithms/PerfectSquares.java
@@ -1,5 +1,26 @@
 // O(n*sqrt(n)) = O(n1.5) space O(n)
 
+// improvement, from large number to small one to check
+public class Solution {
+    public int numSquares(int n) {
+        if(n < 0) return 0;
+        int[] dp = new int[n+1];
+        
+        for(int i = 1; i<=n; i++) {
+            dp[i] = i;
+            for(int j = (int)Math.sqrt(i); j>=1; j--) {
+                if(j*j == n) {
+                    dp[i] = 1;
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], 1 + dp[i - j*j]);
+            }
+        }
+        
+        return dp[n];
+    }
+}
+
 public class Solution {
     public int numSquares(int n) {
         if(n < 0) return 0;

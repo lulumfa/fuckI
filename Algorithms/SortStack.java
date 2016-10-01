c7460ef255f1867dd4a981c2680cd7f3216c284f
fucktheinterview <jzy0706@vip.163.com>
2016-10-01 14:48:51 -0700

    Create SortStack.java

---
+++ Algorithms/SortStack.java
@@ -0,0 +1,34 @@
+package Leetcode;
+
+import java.util.Stack;
+
+public class SortStack {
+	
+	public static void main(String[] args) {
+		SortStack ss = new SortStack();
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(3);
+		stack.push(4);
+		stack.push(3);
+		stack.push(5);
+		stack.push(1);
+		System.out.println(ss.sort(stack));
+	}
+	
+	public Stack<Integer> sort(Stack<Integer> stack) {
+		Stack<Integer> copy = new Stack<Integer>();
+		while(!stack.isEmpty()) {
+			int temp = stack.pop();
+			while(!copy.isEmpty() && copy.peek() > temp) {
+				stack.push(copy.pop());
+			}
+			copy.push(temp);
+		}
+		System.out.println(copy);
+		while(!copy.isEmpty()) {
+			stack.push(copy.pop());
+		}
+		
+		return stack;
+	}
+}

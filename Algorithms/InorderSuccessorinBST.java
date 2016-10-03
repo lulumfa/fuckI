b5e181b3e18aa533c06085796fefe931d6c8fcf4
fucktheinterview <jzy0706@vip.163.com>
2016-10-03 12:41:21 -0700

    Update InorderSuccessorinBST.java

--- Algorithms/InorderSuccessorinBST.java
+++ Algorithms/InorderSuccessorinBST.java
@@ -1,3 +1,22 @@
+// O(lgn) space O(lgn)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if(root == null || p ==null) return null;
+        if(root.val <= p.val) return inorderSuccessor(root.right, p);
+        TreeNode left = inorderSuccessor(root.left, p);
+        return left == null ? root : left;
+    }
+}
+
 //O(n), space O(lgn)
 
 /**

9e4042395b8d976037e76dab3de866eb55f8bf79
fucktheinterview <jzy0706@vip.163.com>
2016-10-02 21:01:27 -0700

    Update BinaryTreePreorderTraversal.java

--- Algorithms/BinaryTreePreorderTraversal.java
+++ Algorithms/BinaryTreePreorderTraversal.java
@@ -1,3 +1,32 @@
+// my latest
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root == null) return res;
+        TreeNode node = root;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while(node != null || !stack.isEmpty()) {
+            if(node != null) {
+                res.add(node.val);
+                if(node.right != null) stack.push(node.right);
+                node = node.left;
+            } else {
+                node = stack.pop();
+            }
+        }
+        return res;
+    }
+}
+
 // Morris
 
 // reference: http://blog.csdn.net/linhuanmars/article/details/21428647

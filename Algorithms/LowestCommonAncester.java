a4e6517345e12a56d1de7f55bdcd3fa1b7bd77a9
fucktheinterview <jzy0706@vip.163.com>
2016-10-03 20:28:17 -0700

    Update LowestCommonAncester.java

--- Algorithms/LowestCommonAncester.java
+++ Algorithms/LowestCommonAncester.java
@@ -1,3 +1,38 @@
+// best way O(n), based on one of the ideas below plus checking whether they are existing first.
+// space O(lgn)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return root;
+        if(!isCovered(root, p) || ! isCovered(root, q)) return null;
+        return findLCA(root, p, q);
+    }
+    
+    private TreeNode findLCA(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(root == p || root == q) return root;
+        TreeNode left = findLCA(root.left, p, q);
+        TreeNode right = findLCA(root.right, p, q);
+        
+        if(left != null && right != null) return root;
+        return left != null ? left : right;
+    }
+    
+    private boolean isCovered(TreeNode root, TreeNode target) {
+        if(root == null) return false;
+        if(root == target) return true;
+        return isCovered(root.left, target) || isCovered(root.right, target);
+    }
+}
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {

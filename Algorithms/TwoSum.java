284dbe28a7cbe91741006dcb05b8c881c3d42f8d
fucktheinterview <jzy0706@vip.163.com>
2016-10-23 21:57:52 -0700

    Update TwoSum.java

--- Algorithms/TwoSum.java
+++ Algorithms/TwoSum.java
@@ -1,3 +1,25 @@
+// latest 
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums == null || nums.length <2) return null;
+        
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i = 0; i< nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                return res;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+}
+
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] res = {0, 0};

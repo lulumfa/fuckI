8b45927b1638878d163b6da6784bd25f5825b6a5
fucktheinterview <jzy0706@vip.163.com>
2016-10-30 10:39:09 -0700

    Update BinaryTreeUpsideDown.java

--- Algorithms/BinaryTreeUpsideDown.java
+++ Algorithms/BinaryTreeUpsideDown.java
@@ -1,3 +1,28 @@
+//recursively O(n), space height of the tree, worst case O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root == null || root.left == null) return root;
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        
+        return newRoot;
+    }
+}
+
 // http://blog.csdn.net/whuwangyi/article/details/43186045
 
 	public TreeNode UpsideDownBinaryTree(TreeNode root) {

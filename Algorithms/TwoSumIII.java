38162d54663e3f4ebb0423cdfada39fae2b067f5
fucktheinterview <jzy0706@vip.163.com>
2016-10-30 17:50:30 -0700

    Update TwoSumIII.java

--- Algorithms/TwoSumIII.java
+++ Algorithms/TwoSumIII.java
@@ -1,3 +1,33 @@
+// more find opertions, add O(n), find O(1)
+
+public class TwoSum {
+    HashSet<Integer> nums;
+    HashSet<Integer> sums;
+    
+    public TwoSum() {
+        nums = new HashSet<Integer>();
+        sums = new HashSet<Integer>();
+    }
+    
+    // Add the number to an internal data structure.
+	public void add(int number) {
+	    if(nums.contains(number)) {
+	        sums.add(number * 2);
+	    } else {
+	        for(Integer num : nums) {
+	            sums.add(num + number);
+	        }
+	    } 
+	    nums.add(number);
+	}
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean find(int value) {
+	    return sums.contains(value);
+	}
+}
+
+
 // add O(1), find(n), tradoff more add operations
 
 public class TwoSum {

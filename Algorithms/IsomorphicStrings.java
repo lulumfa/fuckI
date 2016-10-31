542f4225640dff6569cd783848744cadc5ea5c6e
fucktheinterview <jzy0706@vip.163.com>
2016-10-30 23:00:02 -0700

    Update IsomorphicStrings.java

--- Algorithms/IsomorphicStrings.java
+++ Algorithms/IsomorphicStrings.java
@@ -1,3 +1,27 @@
+//O(n) length of s, t, space O(256) O(1)
+
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s == null || t == null || s.length() != t.length()) return false;
+        
+        Character[] sT = new Character[256];
+        Character[] tS = new Character[256];
+        
+        for(int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            
+            if(sT[sChar] == null && tS[tChar] == null) {
+                sT[sChar] = tChar;
+                tS[tChar] = sChar;
+            } else if(sT[sChar] == null || tS[tChar] == null || sT[sChar] != tChar || tS[tChar] != sChar) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 // O(n), space O(n)
 
 public class Solution {

4816833a7b33edebc9dc8a26572f5c3ccc71a1da
fucktheinterview <jzy0706@vip.163.com>
2016-11-02 19:52:07 -0700

    Update NestedListWeightSum.java

--- Algorithms/NestedListWeightSum.java
+++ Algorithms/NestedListWeightSum.java
@@ -1,5 +1,56 @@
 // iteratively
 // O(n); space (max size of a level) O(n)
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        if(nestedList == null) return 0;
+        
+        Queue<NestedInteger> queue = new LinkedList<NestedInteger>();
+        queue.addAll(nestedList);
+        
+        int pre = nestedList.size();
+        int cur = 0;
+        int level = 1;
+        
+        int sum = 0;
+        
+        while(!queue.isEmpty()) {
+            NestedInteger curElement = queue.poll();
+            pre--;
+            if(curElement.isInteger()) sum += curElement.getInteger() * level;
+            else {
+                for(NestedInteger element : curElement.getList()) {
+                    queue.offer(element);
+                    cur++;
+                }
+            }
+            if(pre == 0) {
+                pre = cur;
+                cur = 0;
+                level++;
+            }
+        }
+        return sum;
+    }
+}
+
 public class Solution {
     public int depthSum(List<NestedInteger> nestedList) {
         if(nestedList == null) return 0;

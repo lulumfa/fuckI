6fdf69b7d07c4ba1c91356cbe522d0c93a31c6c2
fucktheinterview <jzy0706@vip.163.com>
2016-11-03 21:40:39 -0700

    Update FindtheCelebrity.java

--- Algorithms/FindtheCelebrity.java
+++ Algorithms/FindtheCelebrity.java
@@ -5,6 +5,25 @@
 
 public class Solution extends Relation {
     public int findCelebrity(int n) {
+        if(n < 1) return -1;
+        int candidate = 0;
+        
+        for(int i = 1; i < n; i++) {
+            if(!knows(i, candidate)) candidate = i;  // we can use either condition check here
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(i != candidate && (!knows(i, candidate) || knows(candidate, i))) return -1;
+        }
+        return candidate;
+    }
+}
+
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
         if(n<1) return -1;
         int candidate = 0;
         

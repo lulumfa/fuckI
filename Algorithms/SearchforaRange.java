96f640cce05703f2eff3d57746bb5bf288e0fe61
fucktheinterview <jzy0706@vip.163.com>
2016-11-05 13:03:11 -0400

    Update SearchforaRange.java

--- Algorithms/SearchforaRange.java
+++ Algorithms/SearchforaRange.java
@@ -1,3 +1,35 @@
+//faster considering in the failure scenario this only go through it once
+//lgn, constant space
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if(nums == null) return null;
+        
+        int[] res = {-1, -1};
+        
+        // search left bound;
+        int left = 0;
+        int right = nums.length -1;
+        while(left <= right) {
+            int mid = (left + right)/2;
+            if(nums[mid] >= target) right = mid -1;
+            else left = mid + 1;
+        }
+        if((right + 1) >= nums.length || nums[right + 1] != target) return res;
+        res[0] = right+1;
+        
+        // search right bound;
+        left = 0;
+        right = nums.length -1;
+        while(left <= right) {
+            int mid = (left + right)/2;
+            if(nums[mid] > target) right = mid -1;
+            else left = mid + 1;
+        }
+        res[1] = left-1;
+        return res;
+    }
+}
+
 // http://blog.csdn.net/linhuanmars/article/details/20593391
 
 public int[] searchRange(int[] A, int target) {

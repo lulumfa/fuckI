f356316f1bba48eea1480b2252cf9df470b3fc3e
fucktheinterview <jzy0706@vip.163.com>
2016-11-05 16:50:56 -0400

    Update SymmetricTree.java

--- Algorithms/SymmetricTree.java
+++ Algorithms/SymmetricTree.java
@@ -27,6 +27,46 @@ public class Solution {
 
 
 Iterative Solution
+
+// my latest way O(n), space O(n), one queue
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        if(root.left == null && root.right == null) return true;
+        if(root.left == null || root.right == null) return false;
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        
+        while(!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if(left.val != right.val) return false;
+            if(left.left == null && right.right != null || left.left != null && right.right == null || left.right == null && right.left != null || left.right != null && right.left == null) return false;
+            if(left.left != null && right.right != null) {
+                queue.offer(left.left);
+                queue.offer(right.right);
+            }
+            if(left.right != null && right.left != null) {
+                queue.offer(left.right);
+                queue.offer(right.left);
+            }
+        }
+        return true;
+    }
+}
+
 /**
  * Definition for binary tree
  * public class TreeNode {

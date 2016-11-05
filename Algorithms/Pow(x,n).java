96aba036307e7a7a09f1fd76ac3bdf7d04e33da4
fucktheinterview <jzy0706@vip.163.com>
2016-11-05 09:51:52 -0400

    Update Pow(x,n).java

--- Algorithms/Pow(x,n).java
+++ Algorithms/Pow(x,n).java
@@ -1,3 +1,50 @@
+// my latest with my thought
+public class Solution {
+    public double myPow(double x, int n) {
+        // overflow issue with multiplication
+        // n: 7 = '111' = 1*1 + 2*1 + 4*1 => x ^ n = x ^ (1*1 + 2*1 + 4*1) overflow case
+        // 6 = '110' = 1*0 + 2*1+ 4*1
+        // x = 0 ; 1; -3;
+        // n = 0; -2 => x = 1/x overflow case, multiplicative inverse
+        
+        
+        if(x == 0) return 0.;
+        if(x == 1. || n == 0) return 1.; 
+        double res = 1.;
+        
+        if(n < 0) {
+            // invalid case 1/x > Double.MAX_VALUE or 1/x < -Double.MAX_VALUE
+            if(x >= 1./Double.MAX_VALUE || x <= -1./Double.MAX_VALUE) {
+                x = 1./x;
+            } else {
+                return Double.MAX_VALUE;
+            }
+            // abs(Integer.MIN) = abs(Integer.MAX) + 1
+            if(n == Integer.MIN_VALUE) {
+                n++;
+                res *= x;
+            }
+            n = Math.abs(n);
+        }
+        
+        boolean isNeg = x < 0 && (n & 1) == 1;
+        if(x < 0) x = Math.abs(x);
+        
+        double base = x;
+        //'111' : x ^ n = x ^ (1*1 + 2*1 + 4*1)
+        while(n > 0) {
+            if((n & 1) == 1) {
+                if(res  > Double.MAX_VALUE/base) return Double.MAX_VALUE;
+                res *= base;
+            }
+            if(base > Double.MAX_VALUE/base) return Double.MAX_VALUE;
+            base *= base;
+            n >>= 1;
+        }
+        return isNeg ? -res : res;
+    }
+}
+
 // more robust
 //这道题是一道数值计算的题目，因为指数是可以使结果变大的运算，所以要注意越界的问题。如同我在Sqrt(x)这道题中提到的，一般来说数值计算的题目可以用两种方法来解，一种是以2为基进行位处理的方法，另一种是用二分法。这道题这两种方法都可以解，下面我们分别介绍。
 //http://blog.csdn.net/linhuanmars/article/details/20092829

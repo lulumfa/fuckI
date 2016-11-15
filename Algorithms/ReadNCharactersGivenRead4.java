32b245a213bd7b68f3ea862550b3c1cf2cb1b2cf
fucktheinterview <jzy0706@vip.163.com>
2016-11-15 08:45:41 -0800

    Update ReadNCharactersGivenRead4.java

--- Algorithms/ReadNCharactersGivenRead4.java
+++ Algorithms/ReadNCharactersGivenRead4.java
@@ -1,3 +1,28 @@
+// my way, O(n), constant space
+
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        if(buf == null || n < 1) return 0;
+        for(int i = 0; i < n; i+=4) {
+            char[] next = new char[4];
+            int nextSize = read4(next);
+            for(int j = 0; j < Math.min(n - i, nextSize); j++) {
+                buf[i + j] = next[j];
+            }
+            if(nextSize < 4) return Math.min(i + nextSize, n);
+        }
+        return n;
+    }
+}
+
 //http://www.meetqun.com/thread-2812-1-1.html
 
 public class Solution extends Reader4 {

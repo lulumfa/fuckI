a325574b6c25d181eba129314fa982e0a2bb1d2a
fucktheinterview <jzy0706@vip.163.com>
2016-11-14 23:32:52 -0800

    Update PermutationForPhone.java

--- Algorithms/PermutationForPhone.java
+++ Algorithms/PermutationForPhone.java
@@ -1,3 +1,48 @@
+public class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<String>();
+        if(digits == null || digits.length() == 0) return res;
+        dfsFindCombinations(res, digits, 0, new StringBuilder());
+        return res;
+    }
+    
+    private void dfsFindCombinations(List<String> res, String digits, int index, StringBuilder sb) {
+        if(index == digits.length()) {
+            res.add(sb.toString());
+            return;
+        }
+        String chars = findChars(digits.charAt(index)); 
+        for(int i = 0; i < chars.length(); i++) {
+            StringBuilder next = new StringBuilder(sb);
+            next.append(chars.charAt(i));
+            dfsFindCombinations(res, digits, index + 1, next);
+        }
+    }
+    
+    private String findChars(char c) {
+        switch(c) {
+            case '2':
+                return "abc";
+            case '3':
+                return "def";
+            case '4':
+                return "ghi";
+            case '5':
+                return "jkl";
+            case '6':
+                return "mno";
+            case '7':
+                return "pqrs";
+            case '8':
+                return "tuv";
+            case '9':
+                return "wxyz";
+            default:
+                return "";
+        }
+    }
+}
+
 //http://blog.csdn.net/linhuanmars/article/details/19743197
 
 public ArrayList<String> letterCombinations(String digits) {

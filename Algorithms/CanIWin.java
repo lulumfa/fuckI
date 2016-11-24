e16d2b90c2501cc07bea869ed69effd09e71cc2b
fucktheinterview <jzy0706@vip.163.com>
2016-11-24 15:16:03 -0800

    Update CanIWin.java

--- Algorithms/CanIWin.java
+++ Algorithms/CanIWin.java
@@ -1,5 +1,47 @@
 //  m = desiredTotal, n = maxInt ;using memo reduced it from expenential to expenential still O(2^n), but no redundent calls
 
+// save space by using bits if maxInt < 32
+
+public class Solution {
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        if(desiredTotal <=0) return true;
+        long maxChoosableLong = maxChoosableInteger;
+        long sumAll = (maxChoosableInteger + 1) * maxChoosableInteger/2;
+        if(sumAll < (long)desiredTotal) return false;
+        return dfsCheckWithMemorization(desiredTotal, new boolean[maxChoosableInteger], new HashMap<Integer, Boolean>());
+    }
+    
+    private boolean dfsCheckWithMemorization(int desiredTotal, boolean[] used, HashMap<Integer, Boolean> memo) {
+        Integer curKey = calculateKey(used);
+        if(memo.containsKey(curKey)) return memo.get(curKey);
+        
+        for(int i = 0; i < used.length; i++) {
+            if(!used[i]) {
+                used[i] = true;
+                if((i+1) >= desiredTotal || !dfsCheckWithMemorization(desiredTotal - (i+1), used, memo)) {
+                    memo.put(curKey, true);
+                    used[i] = false;
+                    return true;
+                }
+                used[i] = false;
+            }
+        }
+        
+        memo.put(curKey, false);
+        return false;
+    }
+    
+    private int calculateKey(boolean[] used) {
+        int key = 0;
+        for(boolean digitUsed : used) {
+            key <<= 1;
+            key |= digitUsed ? 1 : 0;
+        }
+        return key;
+    }
+}
+
+
 public class Solution {
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
         if(desiredTotal <=0) return true;

18e76fba63155996ec7bff6699f64b6233b38609
fucktheinterview <jzy0706@vip.163.com>
2016-12-11 20:34:19 -0800

    Update WallsandGates.java

--- Algorithms/WallsandGates.java
+++ Algorithms/WallsandGates.java
@@ -8,6 +8,42 @@ public class Solution {
         int m = rooms.length, n = rooms[0].length;
         Queue<Integer> queue = new LinkedList<Integer>();
         
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(rooms[i][j] == 0) {
+                    queue.offer(i * n + j);
+                }
+            }
+        }
+        
+        while(!queue.isEmpty()) {
+            int curPoint = queue.poll();
+            int x = curPoint/n;
+            int y = curPoint%n;
+            for(int[] dir : dirs) {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+                if(isValid(newX, newY, rooms)) {
+                    rooms[newX][newY] = rooms[x][y] + 1;
+                    queue.offer(newX * n + newY);
+                }
+            }
+        }
+    }
+    
+    private boolean isValid(int x, int y, int[][] rooms) {
+        return x >=0 && x < rooms.length && y >=0 && y < rooms[0].length && rooms[x][y] == Integer.MAX_VALUE;  
+    }
+}
+
+public class Solution {
+    private static int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
+    public void wallsAndGates(int[][] rooms) {
+        if(rooms == null || rooms.length == 0 || rooms[0].length == 0) return;
+        int m = rooms.length, n = rooms[0].length;
+        Queue<Integer> queue = new LinkedList<Integer>();
+        
         int pre = 0, cur = 0, level = 1;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {

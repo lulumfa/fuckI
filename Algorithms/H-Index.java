161218142d48be7143d63bdc903757d436687bb4
fucktheinterview <jzy0706@vip.163.com>
2016-12-12 00:12:08 -0800

    Create H-Index.java

---
+++ Algorithms/H-Index.java
@@ -0,0 +1,25 @@
+// O(n), space O(n)
+
+public class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) return 0;
+        int L = citations.length;
+        int[] count = new int[L + 1];
+        
+        for(Integer citation : citations) {
+            if(citation >= L) {
+                count[L]++;
+            } else {
+                count[citation]++;
+            }
+        }
+        
+        int sum = 0;
+        for(int i = L; i >=0; i--) {
+            sum += count[i];
+            if(sum >= i) return i;
+        }
+        
+        return 0;
+    }
+}

7a9a87373192ec63a68b4db7c776bf60a93f6795
fucktheinterview <jzy0706@vip.163.com>
2016-12-24 09:36:17 -0800

    Create H-IndexII.java

---
+++ Algorithms/H-IndexII.java
@@ -0,0 +1,25 @@
+// after sorting
+// O(lgn), space O(1)
+
+public class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) return 0;
+        int n = citations.length;
+        int left = 0, right = n -1;
+        
+        int res = 0;
+        while(left <= right) {
+            int mid = (left + right)/2;
+            if(n - mid == citations[mid]) {
+                return n - mid;
+            } else if(n - mid < citations[mid]) {
+                res = n - mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return res;
+    }
+}

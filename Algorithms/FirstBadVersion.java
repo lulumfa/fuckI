e568a1e1b4d33c649fe959573ffeaebdd1bacbb7
fucktheinterview <jzy0706@vip.163.com>
2016-12-25 22:00:29 -0800

    Update FirstBadVersion.java

--- Algorithms/FirstBadVersion.java
+++ Algorithms/FirstBadVersion.java
@@ -3,16 +3,23 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        if(n < 1) return 0;
-        int left = 1, right = n;
+        if(n<1) return 0;
         
-        while(left < right) {
+        int left = 1, right = n;
+        while(left <= right) {
             int mid = left + (right - left)/2;
-            if(isBadVersion(mid)) right = mid;
-            else left = mid + 1;
+            if(isBadVersion(mid)) {
+                right = mid -1;
+            } else {
+                left = mid + 1;
+            }
         }
+        
         return left;
-    }
+    } 
 }

43b87168886dc26a70b3928fed4f954e3f9867ce
fucktheinterview <jzy0706@vip.163.com>
2016-12-27 22:19:45 -0800

    Update InorderSuccessorinBST.java

--- Algorithms/InorderSuccessorinBST.java
+++ Algorithms/InorderSuccessorinBST.java
@@ -1,13 +1,25 @@
-// O(lgn) space O(lgn), https://discuss.leetcode.com/topic/25076/share-my-java-recursive-solution/2
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+// O(n), space O(1)
+
+//iteratively
+//https://discuss.leetcode.com/topic/25698/java-python-solution-o-h-time-and-o-1-space-iterative
+public class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode succ = null;
+        while (root != null) {
+            if (p.val < root.val) {
+                succ = root;
+                root = root.left;
+            }
+            else
+                root = root.right;
+        }
+        return succ;
+    }
+}
+
+// O(n) space O(n), https://discuss.leetcode.com/topic/25076/share-my-java-recursive-solution/2
+
+// recursively
 public class Solution {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         if(root == null || p ==null) return null;
@@ -15,6 +27,18 @@ public class Solution {
         TreeNode left = inorderSuccessor(root.left, p);
         return left == null ? root : left;
     }
+}
+
+public TreeNode predecessor(TreeNode root, TreeNode p) {
+  if (root == null)
+    return null;
+
+  if (root.val >= p.val) {
+    return predecessor(root.left, p);
+  } else {
+    TreeNode right = predecessor(root.right, p);
+    return (right != null) ? right : root;
+  }
 }
 
 //O(n), space O(lgn)

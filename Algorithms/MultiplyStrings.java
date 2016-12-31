93b5c9d03c5b6a78726a49e639e9e7c6f3782313
fucktheinterview <jzy0706@vip.163.com>
2016-12-31 12:52:58 -0800

    Update MultiplyStrings.java

--- Algorithms/MultiplyStrings.java
+++ Algorithms/MultiplyStrings.java
@@ -1,3 +1,29 @@
+// O(mn), space(m + n)
+public class Solution {
+    public String multiply(String num1, String num2) {
+        
+        // num1[i] * num2[j] = [i + j, i + j + 1]
+        if(num1 == null || num2 == null) return null;
+        int m = num1.length(), n = num2.length();
+        
+        int[] digits = new int[m + n];
+        
+        for(int i = m -1; i >=0; i--) {
+            for(int j = n -1; j >=0; j--) {
+                int mult = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int sum = mult + digits[i + j + 1];
+                
+                digits[i + j] += sum/10;
+                digits[i + j + 1] = sum % 10;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int digit : digits) if(!(sb.length() == 0 && digit == 0)) sb.append(digit);
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
+
 // my own (m+n)^2, space constant
 
 public class Solution {

704955355fd4187bc3a4278a422de6a798c95b79
fucktheinterview <jzy0706@vip.163.com>
2017-01-02 12:50:33 -0800

    Update QuickSort.java

--- DataStructure/QuickSort.java
+++ DataStructure/QuickSort.java
@@ -1,3 +1,39 @@
+// my easy understanding one, worst O(n2), average nlgn, in place
+//快速排序之所比较快，因为相比冒泡排序，每次交换是跳跃式的。每次排序的时候设置一个基准点，
+//将小于等于基准点的数全部放到基准点的左边，将大于等于基准点的数全部放到基准点的右边。
+//这样在每次交换的时候就不会像冒泡排序一样每次只能在相邻的数之间进行交换，交换的距离就大的多了。
+//因此总的比较和交换次数就少了，速度自然就提高了。
+public class Quicksort  {
+	
+	public static void main(String[] args) {
+		Quicksort qs = new Quicksort();
+		System.out.println(Arrays.toString(qs.sort(new int[]{3, 1, 1, 1, 5, 6, 2, 3, 10})));
+	}
+	
+	public int[] sort(int[] input) {
+		if(input == null || input.length == 0) return null;
+		quickSort(input, 0, input.length -1);
+		return input;
+	}
+	
+	public void quickSort(int[] input, int start, int end) {
+		if(start >= end) return;
+		int i = start, j = end, pivot = input[start];
+		
+		while( i < j) {
+			while(i < j && input[j] >= pivot) j--;
+			if(i < j) input[i++] = input[j];
+			
+			while(i < j && input[i] < pivot) i++;
+			if(i < j) input[j--] = input[i];
+		}
+		
+		input[i] = pivot;
+		quickSort(input, start, i -1);
+		quickSort(input, i + 1, end);
+	}
+} 
+
 package Facebook;
 
 import java.util.Arrays;

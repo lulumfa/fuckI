7e5a50b3dec1c0fd1cdafa499bd0906471c04c3b
fucktheinterview <jzy0706@vip.163.com>
2017-01-12 22:26:59 -0800

    Update ReverseNodesinkGroup.java

--- Algorithms/ReverseNodesinkGroup.java
+++ Algorithms/ReverseNodesinkGroup.java
@@ -1,3 +1,26 @@
+// O(n), constant space
+
+// borrowed but really good
+public ListNode reverseKGroup(ListNode head, int k) {
+        int n = 0;
+        for (ListNode i = head; i != null; n++, i = i.next);
+        
+        ListNode dmy = new ListNode(0);
+        dmy.next = head;
+        for(ListNode prev = dmy, tail = head; n >= k; n -= k) {
+            for (int i = 1; i < k; i++) {
+                ListNode next = tail.next.next;
+                tail.next.next = prev.next;
+                prev.next = tail.next;
+                tail.next = next;
+            }
+            
+            prev = tail;
+            tail = tail.next;
+        }
+        return dmy.next;
+    }
+
 // my own
 // O(n), constant space
 /**

98edb0614e5b00fb7cb0a61133abc663427f02e9
fucktheinterview <jzy0706@vip.163.com>
2017-01-14 22:37:12 -0800

    Update SumofLeftLeaves.java

--- Algorithms/SumofLeftLeaves.java
+++ Algorithms/SumofLeftLeaves.java
@@ -1,6 +1,24 @@
 //O(n), space O(height)  = O(n)
 
-
+// iteratively also, but better sequence
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null) return 0;
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        int sum = 0;
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if(node.right != null) stack.push(node.right);
+            if(node.left != null) {
+                if(node.left.left == null && node.left.right == null) sum += node.left.val;
+                else stack.push(node.left);
+            }
+        }
+        return sum;
+    }
+}
  // iteratively
 /**
  * Definition for a binary tree node.

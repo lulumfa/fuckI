a2a8f3489fd94e14bfe558cc002fd78e690d281c
fucktheinterview <jzy0706@vip.163.com>
2017-01-23 21:36:35 -0800

    Update ConvertSortedListBinarySearchTree.java

--- Algorithms/ConvertSortedListBinarySearchTree.java
+++ Algorithms/ConvertSortedListBinarySearchTree.java
@@ -1,4 +1,27 @@
 // reference: http://blog.csdn.net/linhuanmars/article/details/23904937
+
+// O(nlgn), space O(n) actually more readable but slower than the one below
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head==null) return null;
+        return toBST(head,null);
+    }
+    public TreeNode toBST(ListNode head, ListNode tail){
+        ListNode slow = head;
+        ListNode fast = head;
+        if(head==tail) return null;
+
+        while(fast!=tail&&fast.next!=tail){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        TreeNode thead = new TreeNode(slow.val);
+        thead.left = toBST(head,slow);
+        thead.right = toBST(slow.next,tail);
+        return thead;
+    }
+}
+
 //整体过程就是一次中序遍历，时间复杂度是O(n)，空间复杂度是栈空间O(logn)
 /**
  * Definition for singly-linked list.
@@ -26,7 +49,7 @@ public class Solution {
             count++;
             temp = temp.next;
         }
-        ListNode[] list = {head};
+        ListNode[] list = {head}; // put it in a list so that we can update it in the fly
         return helper(list, 0, count-1);
     }
     private TreeNode helper(ListNode[] list, int left, int right) {

c222d295d3789509fddf1844fcc6e2cf426986e1
fucktheinterview <jzy0706@vip.163.com>
2017-01-28 14:43:52 -0800

    Update TargetSum.java

--- Algorithms/TargetSum.java
+++ Algorithms/TargetSum.java
@@ -1,3 +1,36 @@
+// dp, O(sum * n), space O(sum)
+
+public class Solution {
+    
+    //dp[i + 1][k + nums[i] * sgn] += dp[i][k]
+    public int findTargetSumWays(int[] nums, int S) {
+        if(nums == null || nums.length == 0) return 0;
+        
+        int sum = 0;
+        for(Integer num : nums) sum += num;
+        if(S< - sum || S > sum) return 0;
+        
+        int size = sum* 2 + 1;
+        int[] ways = new int[size];
+        ways[0 + sum] = 1;
+        
+        for(int i = 0; i < nums.length; i++) {
+            int[] curWays = new int[size];
+            
+            for(int j = 0; j < size; j++) {
+                if(ways[j] != 0) {
+                    curWays[j + nums[i]] += ways[j];
+                    curWays[j - nums[i]] += ways[j];
+                }
+            }
+            
+            ways = curWays;
+        }
+        
+        return ways[sum + S];
+    }
+}
+
 // O(2^n), space(n)
 
 public class Solution {

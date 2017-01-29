2609affcbb3771d983ee69e07b36d8164aa7ce46
fucktheinterview <jzy0706@vip.163.com>
2017-01-29 11:51:18 -0800

    Update ReverseNodesinkGroup.java

--- Algorithms/ReverseNodesinkGroup.java
+++ Algorithms/ReverseNodesinkGroup.java
@@ -1,47 +1,45 @@
 // O(n), constant space
 
-// borrowed but really good
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
-        public ListNode reverseKGroup(ListNode head, int k) {
-            if (head==null||head.next==null||k<2) return head;
-    
-            ListNode dummy = new ListNode(0);
-            dummy.next = head;
-            
-            ListNode tail = dummy, prev = dummy,temp;
-            int count;
-            while(true){
-                count =k;
-                while(count>0&&tail!=null){
-                    count--;
-                    tail=tail.next;
-                } 
-                if (tail==null) break;//Has reached the end
-            
-                head=prev.next;//for next cycle
-            // prev-->temp-->...--->....--->tail-->....
-            // Delete @temp and insert to the next position of @tail
-            // prev-->...-->...-->tail-->head-->...
-            // Assign @temp to the next node of @prev
-            // prev-->temp-->...-->tail-->...-->...
-            // Keep doing until @tail is the next node of @prev
-                while(prev.next!=tail){
-                    temp=prev.next;//Assign
-                    prev.next=temp.next;//Delete
-                    
-                    temp.next=tail.next;
-                    tail.next=temp;//Insert
-                    
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || k < 2) return head;
+        
+        ListNode pre = new ListNode(0), node = head;
+        pre.next = head;
+        ListNode header = pre;
+        
+        int count = 0;
+        
+        while(node != null) {
+            count++;
+            node = node.next;
+           
+            if(count == k) {
+                ListNode cur = pre.next;
+                
+                while(cur.next != node) {
+                    ListNode temp = cur.next.next;
+                    cur.next.next = pre.next;
+                    pre.next = cur.next;
+                    cur.next = temp;
                 }
                 
-                tail=head;
-                prev=head;
-                
+                pre = cur;
+                count = 0;
             }
-            return dummy.next;
-            
         }
+        
+        return header.next;
     }
+}
 
 public ListNode reverseKGroup(ListNode head, int k) {
         int n = 0;

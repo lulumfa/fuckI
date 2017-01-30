a053c4026bc66b9e87fc14f8fc8b2c922d0548f0
fucktheinterview <jzy0706@vip.163.com>
2017-01-29 17:11:37 -0800

    Update MaximalSquare.java

--- Algorithms/MaximalSquare.java
+++ Algorithms/MaximalSquare.java
@@ -1,3 +1,32 @@
+// O(mn), space enhancement to O(n)
+
+public class Solution {
+    // if(matrix[i][j] == '1') dp[i][j] = 1 + Math.min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+    
+    public int maximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        
+        int m = matrix.length, n = matrix[0].length;
+        
+        int[] dp = new int[n+1];
+        int max = 0;
+        for(int i = 0; i < m; i++) {
+            int[] cur = new int[n+1];
+            
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == '1') {
+                    cur[j + 1] = 1 + Math.min(Math.min(dp[j], dp[j + 1]), cur[j]);
+                    max = Math.max(max, cur[j+1]);
+                }
+            }
+            
+            dp = cur;
+        }
+        
+        return max * max;
+    }
+}
+
 // O(mn), space O(mn)
 
 public class Solution {

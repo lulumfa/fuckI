6844047ccf8fe0d2fa7d6f3f08197b75262ac577
fucktheinterview <jzy0706@vip.163.com>
2017-01-29 20:41:26 -0800

    Update LowestCommonAncestorofaBinarySearchTree.java

--- Algorithms/LowestCommonAncestorofaBinarySearchTree.java
+++ Algorithms/LowestCommonAncestorofaBinarySearchTree.java
@@ -1,3 +1,19 @@
+// O(lgn) - O(n), space O(1)
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || p == null || q == null) return null;
+        
+        TreeNode cur = root;
+        
+        while(cur != null) {
+            if(p.val <= cur.val && q.val >= cur.val || p.val >= cur.val && q.val <= cur.val) return cur;
+            if(p.val < cur.val) cur = cur.left;
+            else cur = cur.right;
+        }
+        return null;
+    }
+}
+
 // O(lgn), space O(lgn)
 /**
  * Definition for a binary tree node.

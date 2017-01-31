a59fcb1cf4e47b2b1a80357d4cb19e8c907da94a
fucktheinterview <jzy0706@vip.163.com>
2017-01-31 12:30:13 -0800

    Update TaskScheduler.java

--- Algorithms/TaskScheduler.java
+++ Algorithms/TaskScheduler.java
@@ -78,3 +78,47 @@ public class TaskScheduler {
 		return sb.toString();
 	}
 }
+
+3，无序的，频率统计的做法，算最后时间
+//if tasks can be reordered, output the minimum time needed: O(n) time, O(n) space
+   private static int taskSchedule3(int[] tasks, int cooldown) {
+       HashMap<Integer, Integer> map = new HashMap<>();
+       for (int task : tasks) {
+           if (!map.containsKey(task)) {
+               map.put(task, 1);
+           } else {
+               map.put(task, map.get(task) + 1);
+           }
+       }
+       int maxFrequency = 0;
+       int countOfMax = 0;
+       for (int frequency : map.values()) {
+           if (frequency > maxFrequency) {
+               maxFrequency = frequency;
+               countOfMax = 1;
+           } else if (frequency == maxFrequency) {
+               countOfMax++;
+           }
+       }
+       int minimumTime = (maxFrequency - 1) * (cooldown + 1) + countOfMax;
+       return Math.max(minimumTime, tasks.length);
+       //(maxFrequency - 1) * (cooldown + 1) + countOfMax;
+       //(maxFrequency - 1): number of minimum time interval; (cooldown + 1): length of each minimum time interval;
+       //countOfMax: the number of tasks at the end (the cooldown of these tasks don't need to be filled)
+       //eg. 1113, cooldown = 0, minimumTime = (3-1)*1 + 1 = 3, task.length = 4, we should return 4
+       //eg. 1123, cooldown = 1, minimumTime = (2-1)*2 + 1 = 3, task.length = 4, we should return 4
+       //eg. 11122, cooldown = 2, minimumTime = (3-1)*3 + 1 = 7 (1 2 _ 1 2 _ 1), task.length = 5, we should return 7
+/**
+    * Find the task that appears for the most time
+    * Use a map to count the number of the times the task appears  then get the maximum count
+    * the result is decided by the maximum count and the number of tasks with maximum count
+    *
+    * two conditions:
+    * 1.  5 4 _ _ _ 5 4 _ _ _ 5 4 _ _ _ 5 4  the rest tasks cannot fill the empty slots
+    *     5 4 3 2 _ 5 4 3 2 _ 5 4 _ _ _ 5 4
+    *     the answer is (maxCount - 1) * (interval + 1) + CountOfMax
+    * 1. 5 4 _ _ _ 5 4 _ _ _ 5 4 _ _ _ 5 4  the rest tasks cannot fill the empty slots
+    *    5 4 3 2 1 6 5 4 3 2 1 6 5 4 6 _ _ 5 4
+    *    the answer is the length of the nums
+    *    the task which does not have max count first fills the empty slots and then just insert any valid place
+    * */

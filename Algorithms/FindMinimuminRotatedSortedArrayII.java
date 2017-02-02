2d880686003de22424e5af58d218099458db825d
fucktheinterview <jzy0706@vip.163.com>
2017-02-01 23:36:44 -0800

    Update FindMinimuminRotatedSortedArrayII.java

--- Algorithms/FindMinimuminRotatedSortedArrayII.java
+++ Algorithms/FindMinimuminRotatedSortedArrayII.java
@@ -1,3 +1,29 @@
+// focus on one side, this way minimum changes from the previous question, still O(n)
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return false;
+        
+        int left = 0, right = nums.length -1;
+        
+        while(left <= right) {
+            int mid = left + (right -left)/2;
+            if(nums[mid] == target) return true;
+            
+            if(nums[left] > nums[mid]) {
+                if(target > nums[mid] && target <= nums[right]) left = mid + 1;
+                else right = mid -1;
+            } else if(nums[mid] > nums[left]){
+                if(target >= nums[left] && target < nums[mid]) right = mid - 1;
+                else left = mid + 1;
+            } else {
+                left++;
+            }
+        }
+        
+        return false;
+    }
+}
+
 //reference: http://blog.csdn.net/linhuanmars/article/details/40449299
 
 public class Solution {

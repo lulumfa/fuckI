3c7f3d0ceacc912622c325503cce6f425dd919b3
fucktheinterview <jzy0706@vip.163.com>
2017-02-02 22:00:06 -0800

    Update BestTimeToBuyAndSellStock.java

--- Algorithms/BestTimeToBuyAndSellStock.java
+++ Algorithms/BestTimeToBuyAndSellStock.java
@@ -1,5 +1,20 @@
 public class Solution {
     public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) return 0;
+        
+        int local = 0, global = 0;
+        
+        for(int i = 1; i < prices.length; i++) {
+            local = Math.max(0, local + prices[i] - prices[i-1]);
+            global = Math.max(local, global);
+        }
+        
+        return global;
+    }
+}
+
+public class Solution {
+    public int maxProfit(int[] prices) {
         if(prices==null || prices.length<=1) return 0;
         int max = 0;
         int sum = 0;

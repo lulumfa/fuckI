89540c6272775453f58c7072e106689df5b4e02d
fucktheinterview <jzy0706@vip.163.com>
2017-02-02 20:51:54 -0800

    Update ValidPalindrome.java

--- Algorithms/ValidPalindrome.java
+++ Algorithms/ValidPalindrome.java
@@ -1,3 +1,25 @@
+// latest, O(n), space O(1)
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == null || s.length() == 0 ) return true;
+        
+        int left = 0, right = s.length() -1;
+        
+        while(left < right) {
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if(left < right) {
+                if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+                left++;
+                right--;
+            }
+        }
+        
+        return true;
+    }
+}
+
 // my latest
 // O(n) space O(1)
 public class Solution {

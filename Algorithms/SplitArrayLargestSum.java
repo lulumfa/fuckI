f2f61cc15e0b712520a58eec2eb429700ff1233d
fucktheinterview <jzy0706@vip.163.com>
2017-02-04 11:01:04 -0800

    Update SplitArrayLargestSum.java

--- Algorithms/SplitArrayLargestSum.java
+++ Algorithms/SplitArrayLargestSum.java
@@ -2,37 +2,41 @@
 
 public class Solution {
     public int splitArray(int[] nums, int m) {
-        int max = 0; long sum = 0;
-        for (int num : nums) {
-            max = Math.max(num, max);
+        if(nums == null || m < 1 || nums.length < m) return 0;
+        
+        int sum = 0, max = 0;
+        for(Integer num : nums) {
             sum += num;
+            max = Math.max(max, num);
         }
-        if (m == 1) return (int)sum;
-        //binary search
-        long l = max; long r = sum;
-        while (l <= r) {
-            long mid = (l + r)/ 2;
-            if (valid(mid, nums, m)) {
-                r = mid - 1;
+        
+        int left = max, right = sum;
+        
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            if(isValid(nums, mid, m)) {
+                right = mid - 1;
             } else {
-                l = mid + 1;
+                left = mid + 1;
             }
         }
-        return (int)l;
+        
+        return left;
     }
-    public boolean valid(long target, int[] nums, int m) {
-        int count = 1;
-        long total = 0;
-        for(int num : nums) {
-            total += num;
-            if (total > target) {
-                total = num;
-                count++;
-                if (count > m) {
-                    return false;
-                }
+    
+    private boolean isValid(int[] nums, int maxSum, int maxSegs) {
+        int sum = 0, segs = 1;
+        
+        for(Integer num : nums) {
+            sum += num;
+            if(sum > maxSum) {
+                sum = num;
+                segs++;
+                
+                if(segs > maxSegs) return false;
             }
         }
+        
         return true;
     }
 }

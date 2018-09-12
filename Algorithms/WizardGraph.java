fc27055f2ad70f557484ab4a01b68c48188b8d8c
fucktheinterview <jzy0706@vip.163.com>
2018-09-11 20:32:11 -0700

    Update WizardGraph.java

--- Algorithms/WizardGraph.java
+++ Algorithms/WizardGraph.java
@@ -1,3 +1,6 @@
+// dijkstra w/ priority queue O(n (logn + E in pq caused by remove)), if the edges are sparse and E is small, then it is close tp nlogn
+// if not using the normal array would do O(n * n to find the min from an array) = O(n2)
+
 package Airbnb;
 
 import java.util.*;
@@ -18,19 +21,65 @@ public class WizardGraph {
 		costList.add(Arrays.asList());
 		
 		WizardGraph wg = new WizardGraph();
+//		System.out.println(wg.findMinCostPQ(costList, 0, 9));
 		System.out.println(wg.findMinCost(costList));
 	}
 	
+	public int findMinCostPQ(List<List<Integer>> costList, int source, int target) {
+		if (costList == null) return Integer.MAX_VALUE;
+		int n = costList.size();
+		Wizard[] wizards = new Wizard[n];
+		Set<Wizard> visited = new HashSet<Wizard>();
+		for (int i = 0; i < n; i++) {
+			wizards[i] = new Wizard(i, i == source ? 0 : Integer.MAX_VALUE);
+		}
+		PriorityQueue<Wizard> pq = new PriorityQueue<Wizard>();
+		pq.offer(wizards[source]);
+		while (!pq.isEmpty()) {
+			Wizard cur = pq.poll();
+			visited.add(cur);
+			if (cur.id == target) {
+				return (int) cur.dist;
+			}
+			for (Integer neighbor : costList.get(cur.id)) {
+				Wizard next = wizards[neighbor];
+				if (!visited.contains(next)) {
+					double newDist = cur.dist + Math.pow(next.id - cur.id, 2); 
+					if (next.dist > newDist) {
+						pq.remove(next);
+						next.dist = newDist;
+						pq.offer(next);
+					}
+				}
+			}
+		}
+		return Integer.MAX_VALUE;
+	}
+	
+	class Wizard implements Comparable<Wizard>{
+		int id;
+		double dist;
+		
+		public Wizard(int id, double dist) {
+			this.id = id;
+			this.dist = dist;
+		}
+		
+		@Override
+		public int compareTo(Wizard that) {
+			return Double.compare(this.dist, that.dist);
+		}
+	}
 	public int findMinCost(List<List<Integer>> costList) {
 		if (costList == null || costList.size() < 10) return Integer.MAX_VALUE;
 		int n = costList.size();
-		Wizard[] wizards = new Wizard[n];
-		for (int i = 0; i < n; i++) wizards[i] = new Wizard(i, i == 0 ? 0 : Integer.MAX_VALUE);
+		Wizard1[] wizards = new Wizard1[n];
+		for (int i = 0; i < n; i++) wizards[i] = new Wizard1(i, i == 0 ? 0 : Integer.MAX_VALUE);
 		
-		Queue<Wizard> queue = new LinkedList<Wizard>();
+		Queue<Wizard1> queue = new LinkedList<Wizard1>();
 		queue.offer(wizards[0]);
 		while(!queue.isEmpty()) {
-			Wizard wizard = queue.poll();
+			Wizard1 wizard = queue.poll();
 			List<Integer> neighbors = costList.get(wizard.idx);
 			for (Integer neighbor : neighbors) {
 				int newDist = wizard.dist + (neighbor - wizard.idx) * (neighbor - wizard.idx);
@@ -44,10 +93,10 @@ public class WizardGraph {
 		return wizards[n-1].dist;
 	}
 	
-	class Wizard {
+	class Wizard1 {
 		int idx;
 		int dist;
-		public Wizard(int idx, int dist) {
+		public Wizard1(int idx, int dist) {
 			this.idx = idx;
 			this.dist = dist;
 		}

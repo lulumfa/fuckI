d75f54dd839053c8538d68940601469359935652
fucktheinterview <jzy0706@vip.163.com>
2018-09-30 11:50:54 -0700

    Update QueueWithArray.java

--- Algorithms/QueueWithArray.java
+++ Algorithms/QueueWithArray.java
@@ -1,13 +1,8 @@
 package Airbnb;
 
+import java.util.*;
+
 public class QueueWithArray<T> {
-	
-	private final static int DEFAULT_SIZE = 10;
-	
-	private final int arraySize;
-	private ListNode<T> head;
-	private ListNode<T> tail;
-	private int size;
 	
 	public static void main(String[] args) {
 		QueueWithArray queue = new QueueWithArray(3);
@@ -32,101 +27,104 @@ public class QueueWithArray<T> {
 		
 		System.out.println(queue.poll());
 		System.out.println(queue.peek());
+		
+		System.out.println(queue.poll());
+		System.out.println(queue.peek());
+		System.out.println(queue.size());
+
+		System.out.println(queue.poll());
+		System.out.println(queue.peek());
 	}
 	
-	public QueueWithArray(int fixedArraySize) {
-		if (fixedArraySize <= 0) {
-			fixedArraySize = DEFAULT_SIZE;
-		}
-		this.arraySize = fixedArraySize;
-	}
+	private static final int DEFAULT_ARRAY_SIZE = 5;
+	private ListNode<T> head;
+	private ListNode<T> tail;
+	private final int arraySize;
+	private int size;
 	
 	public QueueWithArray() {
-		this(DEFAULT_SIZE);
+		this(DEFAULT_ARRAY_SIZE);
 	}
 	
-	public boolean offer(T value) {
-		if (size == 0) {
-			tail = new ListNode<T>(arraySize, value);
-			head = tail;
-		} else if (tail != null) {
-			if (!tail.isFull()) {
-				tail.offer(value);
-			} else {
-				tail.next = new ListNode<T>(arraySize, value);
+	public QueueWithArray(int size) {
+		this.arraySize = size;
+	}
+	
+	public void offer(T value) {
+		if (head == null) {
+			head = new ListNode(arraySize, value);
+			tail = head;
+		} else {
+			if (tail.isFull()) {
+				tail.next = new ListNode(arraySize, value);
 				tail = tail.next;
+				System.out.println("Expanding arrays: " + value);
+			} else {
+				tail.offer(value);
 			}
 		}
+		
 		size++;
-		return true;
 	}
 	
 	public T poll() {
-		if (head == null) return null;
-		
-		T value = head.poll();
-		if (head.isEmpty()) {
-			head = head.next;
-		}
+		if (isEmpty()) return null;
 		size--;
-		if (size == 0) tail = null;
+		T value = head.poll();
+		if (head.isEmpty()) head = head.next;
 		return value;
 	}
 	
 	public T peek() {
-		if (size == 0) return null;
+		if (isEmpty()) return null;
 		return head.peek();
 	}
 	
-	public boolean isEmptry() {
-		return this.size == 0;
+	public boolean isEmpty() {
+		return size() == 0;
 	}
 	
 	public int size() {
 		return this.size;
 	}
 	
+	
 	private class ListNode<T> {
 		T[] list;
 		ListNode<T> next;
+		int head;
+		int tail;
 		
-		int start;
-		int end;
-		
-		public ListNode(int size, T value) {
-			this.list = (T[]) new Object[size];
+		ListNode(int size, T value) {
+			list = (T[]) new Object[size];
 			offer(value);
 		}
 		
-		public boolean offer(T value) {
-			if (end == this.list.length - 1) {
-				return false;
-			}
-			this.list[end++] = value;
-			
-			return true;
+		public void offer(T value) {
+			if (tail == list.length) return;
+			list[tail++] = value;
 		}
 		
 		public T poll() {
-			if (end == start) return null;
-			return this.list[start++];
+			if (isEmpty()) return null;
+			return list[head++];
 		}
 		
 		public T peek() {
-			if (end == start) return null;
-			return this.list[start];
+			if (isEmpty()) return null;
+			return list[head];
+		}
+		
+		public boolean isFull() {
+			return tail == list.length;
+		}
+		
+		public boolean isEmpty() {
+			return head == tail;
 		}
 		
 		public boolean shouldDespose() {
 			return isEmpty() && isFull();
-		}
-		
-		public boolean isEmpty() {
-			return start == end;
-		}
-		
-		public boolean isFull() {
-			return end == this.list.length - 1;
 		}
 	}
 }

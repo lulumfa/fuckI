3cd4bb476ebe38e2af86e497d0e968e075c7d6b7
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-11 09:13:47 -0700

    Update MergeIntervals.java

--- Algorithms/MergeIntervals.java
+++ Algorithms/MergeIntervals.java
@@ -24,6 +24,42 @@ public List<Interval> merge(List<Interval> intervals) {
 }
 }
 
+// O(nlog), space (n), data transformation and result as well
+// changed input format
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) return intervals;
+        
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b) {
+                if (a[0] == b[0]) return a[1] - b[1];
+                return a[0] - b[0];
+            }
+        });
+        
+        List<int[]> list = new ArrayList<int[]>();
+        
+        list.add(intervals[0]);
+        int index =1;
+        int[] cur = intervals[0];
+        while(index < intervals.length) {
+            int[] next = intervals[index];
+            if (next[0] <= cur[1]) cur[1] = Math.max(next[1], cur[1]);
+            else {
+                list.add(next);
+                cur = next;
+            }
+            index++;
+        }
+        int[][] res = new int[list.size()][2];
+        for (int i = 0; i< list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
+
 // reference: http://blog.csdn.net/linhuanmars/article/details/21857617
 
 // 整个算法是先排序，然后再做一次线性遍历，时间复杂度是O(nlogn+n)=O(nlogn)，空间复杂度是O(1)，因为不需要额外空间，只有结果集的空间

a6ae34c15f47c5f00dacc0d33f2f3400d97fc7fc
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-17 10:47:04 -0700

    Update RotateArray.java

--- Algorithms/RotateArray.java
+++ Algorithms/RotateArray.java
@@ -1,6 +1,26 @@
+// (n), space constant, reverse all and then reverse first k, and reverse last n-k
+class Solution {
+  public void rotate(int[] nums, int k) {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+  }
+  public void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+      int temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+  }
+}
+
 // (n), space n, extra space of array
 class Solution {
   public void rotate(int[] nums, int k) {
+    // corner cases check: TODO
     int[] a = new int[nums.length];
     for (int i = 0; i < nums.length; i++) {
       a[(i + k) % nums.length] = nums[i];

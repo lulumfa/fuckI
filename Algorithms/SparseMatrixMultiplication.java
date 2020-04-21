72503b8dd1674ad90af10f1a5e277cb51507e96c
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-21 11:58:50 -0700

    Update SparseMatrixMultiplication.java

--- Algorithms/SparseMatrixMultiplication.java
+++ Algorithms/SparseMatrixMultiplication.java
@@ -1,3 +1,44 @@
+//O(mnk) space result size O(mn)， assuming the sparse factor is huge and
+// the runtime reduced to O(# of non-zero 2d points + m*k scan of first matrix)
+
+//The idea is derived from a CMU lecture.
+// http://www.cs.cmu.edu/~scandal/cacm/node9.html
+// A sparse matrix can be represented as a sequence of rows, 
+// each of which is a sequence of (column-number, value) pairs of the nonzero values in the row.
+
+// So let's create a non-zero array for A, and do multiplication on B.
+
+public int[][] multiply(int[][] A, int[][] B) {
+    int m = A.length, n = A[0].length, nB = B[0].length;
+    int[][] result = new int[m][nB];
+
+    List[] indexA = new List[m];
+    for(int i = 0; i < m; i++) {
+        List<Integer> numsA = new ArrayList<>();
+        for(int j = 0; j < n; j++) {
+            if(A[i][j] != 0){
+                numsA.add(j); 
+                numsA.add(A[i][j]);
+            }
+        }
+        indexA[i] = numsA;
+    }
+
+    for(int i = 0; i < m; i++) {
+        List<Integer> numsA = indexA[i];
+        for(int p = 0; p < numsA.size() - 1; p += 2) {
+            int colA = numsA.get(p);
+            int valA = numsA.get(p + 1);
+            for(int j = 0; j < nB; j ++) {
+                int valB = B[colA][j];
+                result[i][j] += valA * valB;
+            }
+        }
+    }
+
+    return result;   
+}
+
 //O(mnk) space result size O(mn)
 
 //below reduces the calculation of 0 

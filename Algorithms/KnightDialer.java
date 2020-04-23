4a362ba9d28ffe6eb84b8cdca73a555c868e22de
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-23 10:11:49 -0700

    Update KnightDialer.java

--- Algorithms/KnightDialer.java
+++ Algorithms/KnightDialer.java
@@ -5,34 +5,26 @@
 // Space complexity: O(k * 10) -> O(10) = O(1)
 
 class Solution {
-//index -> its neighbor mapping, like a bidirectional graph
-    Integer[][] graph = new Integer[][]{{4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0},
-                                        {},{1, 7, 0}, {2, 6}, {1, 3}, {2, 4}};
-
-    int MOD = (int)1e9 + 7;
     public int knightDialer(int N) {
-        //dp[i][j] stores：when there are 'i' hops left and the current number is 'j', how many combination we have
-        int[][] dp = new int[N + 1][10];
-        //base case, defined by question as: 
-        //when it has 1 hop left on the current number, it should just return 1 (dp[0][j] IS NOT CORRECT)
-        for(int j = 0; j < 10; j++) dp[1][j] = 1;
-        //iterate sub-problem
-        //be careful. i start on 2 because 2 ~ N is N - 1, and i - 1 could get our basecase
-        for(int i = 2; i <= N; i++){
-            for(int j = 0; j < 10; j++){
-                for(int neighbor : graph[j]){//current number's neighbor
-                    dp[i][j] += dp[i - 1][neighbor];
-                    //MOD should be seperated because it may alter our state transfer when number gets big
-                    dp[i][j] %= MOD; 
-                } 
-            
-            }
+        int MOD = 1_000_000_007;
+        int[][] moves = new int[][]{
+            {4,6},{6,8},{7,9},{4,8},{3,9,0},
+            {},{1,7,0},{2,6},{1,3},{2,4}};
+
+        int[][] dp = new int[2][10];
+        Arrays.fill(dp[0], 1);
+        for (int hops = 0; hops < N-1; ++hops) {
+            Arrays.fill(dp[~hops & 1], 0);
+            for (int node = 0; node < 10; ++node)
+                for (int nei: moves[node]) {
+                    dp[~hops & 1][nei] += dp[hops & 1][node];
+                    dp[~hops & 1][nei] %= MOD;
+                }
         }
-        int cnt = 0;
-        for(int j = 0; j < 10;j++){
-            cnt += dp[N][j];
-            cnt %= MOD; //MOD should be seperated 
-        }
-        return cnt;
+
+        long ans = 0;
+        for (int x: dp[~N & 1])
+            ans += x;
+        return (int) (ans % MOD);
     }
 }

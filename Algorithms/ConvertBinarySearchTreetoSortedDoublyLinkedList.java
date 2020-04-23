202f6607f72da8e96c13a7c8c1536bafd52ded7d
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-22 23:43:07 -0700

    Update ConvertBinarySearchTreetoSortedDoublyLinkedList.java

--- Algorithms/ConvertBinarySearchTreetoSortedDoublyLinkedList.java
+++ Algorithms/ConvertBinarySearchTreetoSortedDoublyLinkedList.java
@@ -58,3 +58,34 @@ class Solution {
     return first;
   }
 }
+
+// iterative way
+
+private static Node treeToDoublyList(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Stack<Node> nodeStoreStack = new Stack<>();
+        Node current = root;
+        Node prev = null;
+
+        while (current != null || !nodeStoreStack.isEmpty()) {
+            if (current != null) {
+                nodeStoreStack.push(current);
+                current = current.left;
+            } else {
+                current = nodeStoreStack.pop();
+				if (prev != null) {   // update the left and right pointers accordingly
+                    prev.right = current;
+                    current.left = prev;
+                } else {
+                    root = current;       // save the root node so we can attach the first and last nodes later
+                }
+                prev = current;
+                current = current.right;
+            }
+        }
+        prev.right = root;     // to make the list circular: join the head and tail nodes
+        root.left = prev;       // to make the list circular: join the head and tail nodes
+        return root;
+    }

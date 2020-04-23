b4ba2b39d1406affaba1b55e2351a45ab3e4310f
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-23 10:15:58 -0700

    Update KnightDialer.java

--- Algorithms/KnightDialer.java
+++ Algorithms/KnightDialer.java
@@ -1,30 +1,35 @@
-// O(n),space constant considering limited panel
+// 时间复杂度
+// O(N*10), 因为针对拨号次数循环N次，然后每次步骤的时候需要拨10个号。
 
-//Time complexity: O(k * 10) = O(k)
+// 空间复杂度
+// 总共只需要size是10的数组，所以空间复杂度为常数， O(1)
+    
+// http://www.noteanddata.com/leetcode-935-Knight%20Dialer-java-solution-note.html
 
-// Space complexity: O(k * 10) -> O(10) = O(1)
+ public int knightDialer(int n) {
+    int[][] nextPosTable = {{4,6},{6,8},{7,9},{4,8},{3,9,0}, {},{1,7,0},{6,2}, {1,3},{4,2}};
+    long[] countTable = new long[10];
 
-class Solution {
-    public int knightDialer(int N) {
-        int MOD = 1_000_000_007;
-        int[][] moves = new int[][]{
-            {4,6},{6,8},{7,9},{4,8},{3,9,0},
-            {},{1,7,0},{2,6},{1,3},{2,4}};
-
-        int[][] dp = new int[2][10];
-        Arrays.fill(dp[0], 1);
-        for (int hops = 0; hops < N-1; ++hops) {
-            Arrays.fill(dp[~hops & 1], 0);
-            for (int node = 0; node < 10; ++node)
-                for (int nei: moves[node]) {
-                    dp[~hops & 1][nei] += dp[hops & 1][node];
-                    dp[~hops & 1][nei] %= MOD;
-                }
-        }
-
-        long ans = 0;
-        for (int x: dp[~N & 1])
-            ans += x;
-        return (int) (ans % MOD);
+    for(int i = 0; i < 10; ++i) {
+        countTable[i] = 1;
     }
+    
+    for(int j = 1;j < n; ++j) {
+        long[] nextCountTable = new long[10];
+        for(int i = 0;i  < 10; ++i) {                                            
+            int[] nextPosList = nextPosTable[i];
+            for(int next : nextPosList) {
+                nextCountTable[next] += countTable[i];
+            }
+        }            
+        countTable = nextCountTable;
+        for(int i = 0; i < 10; ++i) {
+            countTable[i] %= 1_000_000_007;
+        }
+    }
+    long sum = 0;
+    for(int i = 0; i < 10; ++i ) {
+        sum += countTable[i];
+    }
+    return (int)(sum%1_000_000_007);
 }

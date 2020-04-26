44a72422ed370cb6d4a7b5dacf04ea75e1c08f97
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-26 01:37:17 -0700

    Update MergeTwoSortedLists.java

--- Algorithms/MergeTwoSortedLists.java
+++ Algorithms/MergeTwoSortedLists.java
@@ -39,3 +39,57 @@ public class Solution {
         return header.next;
     }
 }
+
+// iteractor
+
+public static List<Integer> merge(List<Integer> listA, List<Integer> listB)
+    {
+        if (listA == null) return listB;
+        if (listB == null) return listA;
+
+        final ListIterator<Integer> iterA = listA.listIterator(0);
+        final ListIterator<Integer> iterB = listB.listIterator(0);
+
+        final List<Integer> result = new LinkedList<Integer>();
+    
+        merge(iterA, iterB, result);
+        return result;
+    }
+
+    private static void merge(ListIterator<Integer> iterA,
+                              ListIterator<Integer> iterB,
+                              List<Integer>  result)
+    {
+        if (!iterA.hasNext())
+        {
+            while (iterB.hasNext()) { result.add(iterB.next()); }
+            return;
+        }
+
+        if (!iterB.hasNext())
+        {
+            while (iterA.hasNext()) {result.add(iterA.next()); }
+            return;
+        }
+
+        Integer a = iterA.next();
+        Integer b = iterB.next();
+
+        if (a < b)
+        {
+            result.add(a);
+            b = iterB.previous(); // rewind
+        }
+        else if (b < a)
+        {
+            result.add(b);
+            a = iterA.previous(); // rewind
+        }
+        else
+        {
+            result.add(a);
+            result.add(b);
+        }
+
+        merge(iterA, iterB, result);
+    }

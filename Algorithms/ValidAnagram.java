104cdf0d52a4786e6f0a5e870b9c6107cd416dfb
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-27 10:09:28 -0700

    Update ValidAnagram.java

--- Algorithms/ValidAnagram.java
+++ Algorithms/ValidAnagram.java
@@ -1,3 +1,29 @@
+// O(n), space (1) 26 or 256, 512 ascII 
+
+// or even large 2 bytes (16 bits 2^16) to cover size of unicode
+// Use a hash table instead of a fixed size counter. Imagine allocating a large size array to fit the entire 
+// range of unicode characters, which could go up to more than 1 million. A hash table is a more generic solution 
+// and could adapt to any range of characters.
+
+// hashmap solution below
+
+public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+        return false;
+    }
+    int[] table = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+        table[s.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < t.length(); i++) {
+        table[t.charAt(i) - 'a']--;
+        if (table[t.charAt(i) - 'a'] < 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 // assuming 26 characters only
 public class Solution {
     public boolean isAnagram(String s, String t) {

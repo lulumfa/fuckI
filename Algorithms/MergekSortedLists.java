9be18952d220ae735ed86e7a912714d1dc24265c
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-28 12:43:26 -0700

    Update MergekSortedLists.java

--- Algorithms/MergekSortedLists.java
+++ Algorithms/MergekSortedLists.java
@@ -309,3 +309,72 @@ private ListNode merge(ListNode l1, ListNode l2)
         cur.next = l2;
     return dummy.next;
 }
+
+
+// merging iterator / stream, nklogk for merging everything 
+// mext() O(k) hasNext() O(1)
+
+package fb;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class MergeStream {
+
+  List<Stream<Integer>> list;
+  PriorityQueue<StreamComposite> queue;
+
+  // klogk
+  public MergeStream(List<Stream<Integer>> list) {
+    this.list = list;
+    queue = new PriorityQueue<>(new Comparator<StreamComposite>() {
+      @Override
+      public int compare(StreamComposite s1, StreamComposite s2) {
+        return s1.value - s2.value;
+      }
+    });
+
+    for (Stream<Integer> stream : list) {
+      if (stream.hasNext()) {
+        queue.offer(new StreamComposite(stream, stream.getNext()));
+      }
+    }
+  }
+
+
+  // O(1)
+
+  public boolean hasNext() {
+    return !queue.isEmpty();
+
+  }
+
+  // log k
+  public Integer getNext() {
+    StreamComposite composite = queue.poll();
+    int result = composite.value;
+
+    if (composite.stream.hasNext()) {
+      composite.value = composite.stream.getNext();
+      queue.offer(composite);
+    }
+    return result;
+  }
+}
+
+class StreamComposite {
+  Stream<Integer> stream;
+  int value;
+
+  public StreamComposite(Stream<Integer> stream, int value) {
+    this.stream = stream;
+    this.value = value;
+  }
+}
+
+interface Stream <T> {
+  public boolean hasNext();
+
+  public T getNext();
+}

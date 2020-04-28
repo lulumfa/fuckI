7e74bbc8ea4a048d7311e334ce7f9a9671d58e96
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-27 23:00:14 -0700

    Create EnglishWordToInteger.java

---
+++ Algorithms/EnglishWordToInteger.java
@@ -0,0 +1,6 @@
+// scan from left to right, maintaining current num (before 100), whenever seeing key words
+// like 'Billion', 'Million', 'Thousand', 'Hundred', num * keyword and num = 0;
+
+also maintain the reversed constant map as https://leetcode.com/problems/integer-to-english-words/
+
+https://www.1point3acres.com/bbs/thread-623771-1-1.html

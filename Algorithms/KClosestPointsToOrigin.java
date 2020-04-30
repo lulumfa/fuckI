cf6714517827afa655724aa269d7a0d71e02dd31
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-30 13:42:37 -0700

    Create KClosestPointsToOrigin.java

---
+++ Algorithms/KClosestPointsToOrigin.java
@@ -0,0 +1,79 @@
+// the k elements returned can be any sequence.
+
+// nlogk, space (k)
+
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        if(K == points.length) return points;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>(K, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]);
+            }
+        });
+        
+        for(int[] point: points) {
+            pq.add(point);
+            if(pq.size() > K) pq.poll();
+        }
+        return pq.toArray(new int[0][0]);
+    }
+}
+
+
+// quick select to have better performance in reality
+// O(n) if randomized the pivot, worst case O(n2), space O(k)
+
+
+import java.util.concurrent.ThreadLocalRandom;
+
+class Solution {
+    int[][] points;
+    public int[][] kClosest(int[][] points, int K) {
+        this.points = points;
+        sort(0, points.length - 1, K);
+        return Arrays.copyOfRange(points, 0, K);
+    }
+
+    public void sort(int i, int j, int K) {
+        if (i >= j) return;
+        int k = ThreadLocalRandom.current().nextInt(i, j);
+        swap(i, k);
+
+        int mid = partition(i, j);
+        int leftLength = mid - i + 1;
+        if (K < leftLength)
+            sort(i, mid - 1, K);
+        else if (K > leftLength)
+            sort(mid + 1, j, K - leftLength);
+    }
+
+    public int partition(int i, int j) {
+        int oi = i;
+        int pivot = dist(i);
+        i++;
+
+        while (true) {
+            while (i < j && dist(i) < pivot)
+                i++;
+            while (i <= j && dist(j) > pivot)
+                j--;
+            if (i >= j) break;
+            swap(i, j);
+        }
+        swap(oi, j);
+        return j;
+    }
+
+    public int dist(int i) {
+        return points[i][0] * points[i][0] + points[i][1] * points[i][1];
+    }
+
+    public void swap(int i, int j) {
+        int t0 = points[i][0], t1 = points[i][1];
+        points[i][0] = points[j][0];
+        points[i][1] = points[j][1];
+        points[j][0] = t0;
+        points[j][1] = t1;
+    }
+}

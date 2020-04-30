876defde6c275f3c7afc062722f85d52b8b1fefc
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-04-30 14:06:30 -0700

    Create RangeSumofBST.java

---
+++ Algorithms/RangeSumofBST.java
@@ -0,0 +1,20 @@
+// O(n), stack O(n)
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        int ans = 0;
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node != null) {
+                if (L <= node.val && node.val <= R)
+                    ans += node.val;
+                if (L < node.val)
+                    stack.push(node.left);
+                if (node.val < R)
+                    stack.push(node.right);
+            }
+        }
+        return ans;
+    }
+}

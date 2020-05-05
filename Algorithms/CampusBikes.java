e9ffad36e3b5c631bac1346a0075924908ddbc0f
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-05-05 11:17:05 -0700

    Create CampusBikes.java

---
+++ Algorithms/CampusBikes.java
@@ -0,0 +1,61 @@
+// https://leetcode.com/problems/campus-bikes/discuss/305603/Java-Fully-Explained
+
+//As the question states, there are n workers and m bikes, and m > n.
+We are able to solve this question using a greedy approach.
+
+initiate a priority queue of bike and worker pairs. The heap order should be Distance ASC, WorkerIndex ASC, Bike ASC
+Loop through all workers and bikes, calculate their distance, and then throw it to the queue.
+Initiate a set to keep track of the bikes that have been assigned.
+initiate a result array and fill it with -1. (unassigned)
+poll every possible pair from the priority queue and check if the person already got his bike or the bike has been assigned.
+early exist on every people got their bike.
+
+// O(M*N log(M*N)), space O((M*N)
+
+public int[] assignBikes(int[][] workers, int[][] bikes) {
+        int n = workers.length;
+        
+        // order by Distance ASC, WorkerIndex ASC, BikeIndex ASC
+        PriorityQueue<int[]> q = new PriorityQueue<int[]>((a, b) -> {
+            int comp = Integer.compare(a[0], b[0]);
+            if (comp == 0) {
+                if (a[1] == b[1]) {
+                    return Integer.compare(a[2], b[2]);
+                }
+                
+                return Integer.compare(a[1], b[1]);
+            }
+            
+            return comp;
+        });
+            
+        // loop through every possible pairs of bikes and people,
+        // calculate their distance, and then throw it to the pq.
+        for (int i = 0; i < workers.length; i++) {
+            
+            int[] worker = workers[i];
+            for (int j = 0; j < bikes.length; j++) {
+                int[] bike = bikes[j];
+                int dist = Math.abs(bike[0] - worker[0]) + Math.abs(bike[1] - worker[1]);
+                q.add(new int[]{dist, i, j}); 
+            }
+        }
+        
+        // init the result array with state of 'unvisited'.
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        
+        // assign the bikes.
+        Set<Integer> bikeAssigned = new HashSet<>();
+        while (bikeAssigned.size() < n) {
+            int[] workerAndBikePair = q.poll();
+            if (res[workerAndBikePair[1]] == -1 
+                && !bikeAssigned.contains(workerAndBikePair[2])) {   
+                
+                res[workerAndBikePair[1]] = workerAndBikePair[2];
+                bikeAssigned.add(workerAndBikePair[2]);
+            }
+        }
+        
+        return res;
+    }

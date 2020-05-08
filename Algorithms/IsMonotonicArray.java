65ec1c7a17b79b1722c7cf0f37f3d30bcc0a8a65
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-05-07 23:13:26 -0700

    Create IsMonotonicArray.java

---
+++ Algorithms/IsMonotonicArray.java
@@ -0,0 +1,16 @@
+// O(n), space O(1)
+// one pass
+class Solution {
+    public boolean isMonotonic(int[] A) {
+        boolean increasing = true;
+        boolean decreasing = true;
+        for (int i = 0; i < A.length - 1; ++i) {
+            if (A[i] > A[i+1])
+                increasing = false;
+            if (A[i] < A[i+1])
+                decreasing = false;
+        }
+
+        return increasing || decreasing;
+    }
+}

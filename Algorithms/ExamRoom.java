7734c341a70c0fc79bb3d16af1e68bf4890f08a0
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-05-09 23:09:41 -0700

    Update ExamRoom.java

--- Algorithms/ExamRoom.java
+++ Algorithms/ExamRoom.java
@@ -79,4 +79,22 @@ class Point {
         this.x = x;
         this.y = y;
     }
+    
+    @Override
+    public boolean equals(Object o) {
+          // If the object is compared with itself then return true   
+        if (o == this) { 
+            return true; 
+        } 
+  
+        /* Check if o is an instance of Complex or not 
+          "null instanceof [type]" also returns false */
+        if (!(o instanceof Point)) { 
+            return false; 
+        } 
+          
+        // typecast o to Point so that we can compare data members  
+        Point c = (Point) o; 
+        return this.x == c.x && this.y == c.y;
+    }
 }

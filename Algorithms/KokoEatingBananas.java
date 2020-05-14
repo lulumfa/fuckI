7fb81cfb84d7bb5372e00ea2aa4763ba30f00a0e
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-05-14 01:21:14 -0700

    Create KokoEatingBananas.java

---
+++ Algorithms/KokoEatingBananas.java
@@ -0,0 +1,39 @@
+// O(nlogm), n is # of piles, m is max size of a pile
+// space (1)
+
+  class Solution {
+  public int minEatingSpeed(int[] piles, int H) {
+        int lo = 1, hi = getMaxPile(piles);
+        
+        // Binary search to find the smallest valid K.
+        while (lo <= hi) {
+            int K = lo + ((hi - lo) >> 1);
+            if (canEatAll(piles, K, H)) {
+                hi = K - 1;
+            } else {
+                lo = K + 1;
+            }
+        }
+        
+        return lo;
+    }
+    
+    private boolean canEatAll(int[] piles, int K, int H) {
+        int countHour = 0; // Hours take to eat all bananas at speed K.
+        
+        for (int pile : piles) {
+            countHour += pile / K;
+            if (pile % K != 0)
+                countHour++;
+        }
+        return countHour <= H;
+    }
+    
+    private int getMaxPile(int[] piles) {
+        int maxPile = Integer.MIN_VALUE;
+        for (int pile : piles) {
+            maxPile = Math.max(pile, maxPile);
+        }
+        return maxPile;
+    }
+}

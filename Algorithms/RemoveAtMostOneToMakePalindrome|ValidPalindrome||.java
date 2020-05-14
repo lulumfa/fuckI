65cd4491ec33ede86dcd22e83520b4dafe90ba74
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-05-14 00:55:02 -0700

    Create RemoveAtMostOneToMakePalindrome|ValidPalindrome||.java

---
+++ Algorithms/RemoveAtMostOneToMakePalindrome|ValidPalindrome||.java
@@ -0,0 +1,26 @@
+// O(n), space (1)
+class Solution {
+    public boolean validPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+        
+        int left = 0, right = s.length() -1;
+        
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right -1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    private boolean isPalindrome(String s, int left, int right) {
+        if (s == null || s.length() == 0 || left >= right) return true;
+        
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        return true;
+    }
+}

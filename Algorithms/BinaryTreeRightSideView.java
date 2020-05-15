47d964e4529d319c73fb50b3deb50a2b0660e44b
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-05-14 22:51:29 -0700

    Update BinaryTreeRightSideView.java

--- Algorithms/BinaryTreeRightSideView.java
+++ Algorithms/BinaryTreeRightSideView.java
@@ -1,4 +1,4 @@
-// O(n), space O(n)
+// bfs O(n), space O(n)
 
 /**
  * Definition for a binary tree node.
@@ -43,4 +43,32 @@ class Solution {
         return rightSideList;
     }
 }
-© 2020 GitHub, 
+
+// DFS, O(n), space O(depth = n)
+/*
+The core idea of this algorithm:
+
+1.Each depth of the tree only select one node.
+2. View depth is current size of result list.
+*/
+
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
+    }
+    
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null){
+            return;
+        }
+        if(currDepth == result.size()){
+            result.add(curr.val);
+        }
+        
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+        
+    }
+}

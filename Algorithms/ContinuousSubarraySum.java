6e249418bd19befe90ffe47cbfb0b8817807506f
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-05-15 23:20:19 -0700

    Update ContinuousSubarraySum.java

--- Algorithms/ContinuousSubarraySum.java
+++ Algorithms/ContinuousSubarraySum.java
@@ -1,5 +1,5 @@
 //We iterate through the input array exactly once, keeping track of the running sum mod k of the elements in the process. If we find that a running sum value at index j has been previously seen before in some earlier index i in the array, then we know that the sub-array (i,j] contains a desired sum.
-
+// O(n), space O(n)
 public boolean checkSubarraySum(int[] nums, int k) {
     Map<Integer, Integer> map = new HashMap<Integer, Integer>(){{put(0,-1);}};;
     int runningSum = 0;

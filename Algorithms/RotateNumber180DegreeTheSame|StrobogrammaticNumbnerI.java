fb9c6a7bdd3bd6f27eb975e2c176300d9ffc3cda
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-05-20 09:44:48 -0700

    Update RotateNumber180DegreeTheSame|StrobogrammaticNumbnerI.java

--- Algorithms/RotateNumber180DegreeTheSame|StrobogrammaticNumbnerI.java
+++ Algorithms/RotateNumber180DegreeTheSame|StrobogrammaticNumbnerI.java
@@ -1,18 +1,6 @@
 // O(n), space O(1)
 
 public boolean isStrobogrammatic(String num) {
-        // number's pair. 0 - 0, 1 - 1, 6 - 9, 8 - 8, others -1
-        char[] map = new char[]{'0', '1', 'n', 'n', 'n', 'n', '9', 'n', '8', '6'};
-        
-        for (int i = 0; i < num.length(); i++) {
-            if (map[num.charAt(i) - '0'] != num.charAt(num.length() - 1 - i)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-public boolean isStrobogrammatic(String num) {
     HashMap map = new HashMap();
     map.put('0', '0');
     map.put('1', '1');
@@ -26,3 +14,17 @@ public boolean isStrobogrammatic(String num) {
     }
     return true;
 }
+
+// probably not right, in the case "4", it did not check middle digit
+public boolean isStrobogrammatic(String num) {
+        // number's pair. 0 - 0, 1 - 1, 6 - 9, 8 - 8, others -1
+        char[] map = new char[]{'0', '1', 'n', 'n', 'n', 'n', '9', 'n', '8', '6'};
+        
+        for (int i = 0; i < num.length(); i++) {
+            if (map[num.charAt(i) - '0'] != num.charAt(num.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+

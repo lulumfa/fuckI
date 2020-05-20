d65bc9330fe406878fb4b8292dec3f8db6dffc6b
lulumfa <63165687+lulumfa@users.noreply.github.com>
2020-05-19 22:55:48 -0700

    Update MinimumKnightMoves.java

--- Algorithms/MinimumKnightMoves.java
+++ Algorithms/MinimumKnightMoves.java
@@ -1,3 +1,7 @@
+// constatnt time
+// https://leetcode.com/problems/minimum-knight-moves/discuss/392053/Here-is-how-I-get-the-formula-(with-graphs)
+
+
 // O(2D size n^2), same as size of the queue, whole n^2
 
 //As hint suggests we can simulate all steps because the limits for the possible x and y are low (+-300). Also good catch is to work with abs(x) and abs(y) - it makes code simpler and doesn't affect the answer - just imagine that it's a mirrored image in case of negative x and y.
@@ -48,6 +52,47 @@ class Solution {
             //after we done with all moves from all points stored in te queue at the beggining -
             //increment the step counter
             res++;
+        }
+        return -1;
+    }
+}
+
+// w/o  optimization of space traversal, the method above will only focus on one direction
+
+class Solution {
+    private final int[][] DIRECTIONS = new int[][] {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+    
+    public int minKnightMoves(int x, int y) {
+        x = Math.abs(x);
+        y = Math.abs(y);
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {0, 0});
+        
+        Set<String> visited = new HashSet<>();
+        visited.add("0,0");
+        
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.remove();
+                int curX = cur[0];
+                int curY = cur[1];
+                if (curX == x && curY == y) {
+                    return result;
+                }
+                
+                for (int[] d : DIRECTIONS) {
+                    int newX = curX + d[0];
+                    int newY = curY + d[1];
+                    if (!visited.contains(newX + "," + newY) && newX >= -1 && newY >= -1) {
+                        queue.add(new int[] {newX, newY});
+                        visited.add(newX + "," + newY);
+                    }
+                }
+            }
+            result++;
         }
         return -1;
     }
